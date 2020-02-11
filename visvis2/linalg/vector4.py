4989bd5184f57438db6493b32303d94d4a899d54
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2020-02-11 11:03:13 +0100

    snake_case

--- visvis2/linalg/vector4.py
+++ visvis2/linalg/vector4.py
@@ -18,3 +18,17 @@ class Vector4:
         self.z = z
         self.w = w
         return self
+
+    def apply_matrix4(self, m: "Matrix4") -> "Vector4":
+		x = self.x
+        y = self.y
+        z = self.z
+        w = self.w
+		e = m.elements
+
+		self.x = e[ 0 ] * x + e[ 4 ] * y + e[ 8 ] * z + e[ 12 ] * w
+		self.y = e[ 1 ] * x + e[ 5 ] * y + e[ 9 ] * z + e[ 13 ] * w
+		self.z = e[ 2 ] * x + e[ 6 ] * y + e[ 10 ] * z + e[ 14 ] * w
+		self.w = e[ 3 ] * x + e[ 7 ] * y + e[ 11 ] * z + e[ 15 ] * w
+
+		return self
