775d0d1b26b2301a8e3b1563838af6d26de6bc08
Almar Klein <almar.klein@gmail.com>
2020-02-27 12:40:35 +0100

    Default implementation for get_wgpu_info on base Material class.

--- visvis2/material/_base.py
+++ visvis2/material/_base.py
@@ -1,6 +1,8 @@
 from python_shader import Struct, vec2, mat4
 import numpy as np
 
+from .._wrappers import BufferWrapper
+
 
 # Definition of struct with standard info,
 # provided to each world-object as uniform at slot 0.
@@ -27,6 +29,10 @@ class Material:
         """ Return a list of high level pipeline descriptions.
         These can be compute or render pipelines.
 
+        The default implementation returns a single render pipeline description,
+        using vertex_shader, fragment_shader, and primitive_topology from the material,
+        and index and vertex buffers from the geometry.
+
         Example compute pipeline:
 
             {
@@ -48,4 +54,44 @@ class Material:
                 "target": texture2,  # optional, not-implemented
             },
         """
-        raise NotImplementedError()
+
+        # Get stuff from material
+
+        vertex_shader = self.vertex_shader
+        fragment_shader = self.fragment_shader
+        primitive_topology = self.primitive_topology
+
+        # Get stuff from geometry
+
+        geometry = obj.geometry
+
+        # Use index buffer if present on the geometry
+        index_buffer = getattr(geometry, "index", None)
+        index_buffer = index_buffer if isinstance(index_buffer, BufferWrapper) else None
+
+        # All buffer objects are considered vertex buffers
+        vertex_buffers = [
+            val
+            for val in geometry.__dict__.values()
+            if isinstance(val, BufferWrapper) and val is not index_buffer
+        ]
+        if not vertex_buffers:
+            raise ValueError("Cannot get default wgpu_info: no vertex buffers found.")
+
+        if index_buffer:
+            n = len(index_buffer.data)
+        else:
+            n = len(vertex_buffers[0].data)
+
+        # Put it together!
+
+        return [
+            {
+                "vertex_shader": vertex_shader,
+                "fragment_shader": fragment_shader,
+                "primitive_topology": primitive_topology,
+                "indices": (range(n), range(1)),
+                "index_buffer": index_buffer,
+                "vertex_buffers": vertex_buffers,
+            }
+        ]
--- visvis2/material/_mesh_basic.py
+++ visvis2/material/_mesh_basic.py
@@ -2,25 +2,23 @@ from visvis2.material._base import Material, stdinfo_type
 
 import wgpu  # only for flags/enums
 import python_shader
-from python_shader import vec4, Array
+from python_shader import vec4
 
-# todo: we should probably just use vertex buffers here,
-# but lets make sure to show this technique in another example.
+# todo: put in an example somewhere how to use storage buffers for vertex data:
+# index: (python_shader.RES_INPUT, "VertexId", "i32")
+# positions: (python_shader.RES_BUFFER, (1, 0), "Array(vec4)")
+# position = positions[index]
 
 
 @python_shader.python2shader
 def vertex_shader(
     # input and output
-    index: (python_shader.RES_INPUT, "VertexId", "i32"),
-    # position: (python_shader.RES_INPUT, 0, vec4),
+    position: (python_shader.RES_INPUT, 0, vec4),
     out_pos: (python_shader.RES_OUTPUT, "Position", vec4),
     # uniform and storage buffers
     stdinfo: (python_shader.RES_UNIFORM, (0, 0), stdinfo_type),
-    positions: (python_shader.RES_BUFFER, (1, 0), Array(vec4)),
 ):
-    # pos3 = position.xyz
-    pos3 = positions[index].xyz
-    world_pos = stdinfo.world_transform * vec4(pos3, 1.0)
+    world_pos = stdinfo.world_transform * vec4(position.xyz, 1.0)
     ndc_pos = stdinfo.projection_transform * stdinfo.cam_transform * world_pos
 
     out_pos = ndc_pos  # noqa - shader assign to input arg
@@ -32,21 +30,8 @@ def fragment_shader(out_color: (python_shader.RES_OUTPUT, 0, vec4),):
 
 
 class MeshBasicMaterial(Material):
-    def get_wgpu_info(self, wobject):
-
-        geometry = wobject.geometry
-
-        n = len(geometry.index.data)  # number of faces
-
-        return [
-            {
-                "vertex_shader": vertex_shader,
-                "fragment_shader": fragment_shader,
-                "primitive_topology": wgpu.PrimitiveTopology.triangle_list,
-                "indices": range(n),
-                "index_buffer": geometry.index,
-                # "vertex_buffers": [geometry.positions],
-                "bindings1": [geometry.positions],
-                "target": None,  # default
-            },
-        ]
+    def __init__(self):
+        super().__init__()
+        self.vertex_shader = vertex_shader
+        self.fragment_shader = fragment_shader
+        self.primitive_topology = wgpu.PrimitiveTopology.triangle_list
