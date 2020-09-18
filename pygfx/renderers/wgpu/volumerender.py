8e76852d2b245c7b77cc416b706929b24803f508
Almar Klein <almar.klein@gmail.com>
2020-09-18 22:50:24 +0200

    wip, volumeslice working (but needs polishing)

--- pygfx/renderers/wgpu/volumerender.py
+++ pygfx/renderers/wgpu/volumerender.py
@@ -42,9 +42,8 @@ def volume_slice_renderer(wobject, render_info):
             raise TypeError("material.map must a 3D texture view")
         elif getattr(geometry, "texcoords", None) is None:
             raise ValueError("material.map is present, but geometry has no texcoords")
-        # todo: check that texcoords is 3D, but the Buffer has no API for it yet
-        bindings1[3] = wgpu.BindingType.sampler, material.map
-        bindings1[4] = wgpu.BindingType.sampled_texture, material.map
+        bindings1[2] = wgpu.BindingType.sampler, material.map
+        bindings1[3] = wgpu.BindingType.sampled_texture, material.map
         # Use a version of the shader for float textures if necessary
         if "float" in material.map.format:
             if not hasattr(fragment_shader, "float_version"):
@@ -87,13 +86,14 @@ def vertex_shader_volume_slice(
     # vertices. These are laid out like this:
     #
     #   Vertices       Planes (right, left, back, front, top, bottom)
+    #                            0      1    2      3     4     5
     #
-    #    5----0        0: 0231
-    #   /|   /|        1: 7546
-    #  7----2 |        2: 5014
-    #  | 4--|-1        3: 2763
-    #  |/   |/         4: 0572
-    #  6----3          5: 3641
+    #    5----0        0: 0231        +----+
+    #   /|   /|        1: 7546       /|24 /|
+    #  7----2 |        2: 5014      +----+ |0
+    #  | 4--|-1        3: 2763     1| +--|-+
+    #  |/   |/         4: 0572      |/35 |/
+    #  6----3          5: 3641      +----+
 
     ##
 
@@ -124,18 +124,33 @@ def vertex_shader_volume_slice(
         [0, 3],
         [0, 5],
         [0, 2],
-        [1, 2],
-        [1, 4],
-        [1, 3],
         [1, 5],
+        [1, 3],
+        [1, 4],
+        [1, 2],
         [2, 4],
         [2, 5],
         [3, 4],
         [3, 5],
     ]
 
-    intersect_flag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    intersect_pos = [
+    # Init intersection info
+    intersect_flags = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    intersect_positions = [
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+    ]
+    intersect_texcoords = [
         vec3(0, 0, 0),
         vec3(0, 0, 0),
         vec3(0, 0, 0),
@@ -155,13 +170,33 @@ def vertex_shader_volume_slice(
         edge = edges[i]
         p1 = buf_positions[edge[0]].xyz
         p2 = buf_positions[edge[1]].xyz
+        tc1 = vec3(
+            buf_texcoords[edge[0] * 3],
+            buf_texcoords[edge[0] * 3 + 1],
+            buf_texcoords[edge[0] * 3 + 2],
+        )
+        tc2 = vec3(
+            buf_texcoords[edge[1] * 3],
+            buf_texcoords[edge[1] * 3 + 1],
+            buf_texcoords[edge[1] * 3 + 2],
+        )
         u = p2 - p1
         t = -(plane.x * p1.x + plane.y * p1.y + plane.z * p1.z + plane.w) / (n @ u)
         intersects = i32(t > 0.0) * i32(t < 1.0)
-        intersect_pos[i] = mix(p1, p2, t)
-        intersect_flag[i] = intersects
+        intersect_flags[i] = intersects
+        intersect_positions[i] = mix(p1, p2, t)
+        intersect_texcoords[i] = mix(tc1, tc2, t)
 
+    # Init six vertices
     vertices = [
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 0),
+    ]
+    texcoords = [
         vec3(0, 0, 0),
         vec3(0, 0, 0),
         vec3(0, 0, 0),
@@ -171,58 +206,64 @@ def vertex_shader_volume_slice(
     ]
 
     # Find first intersection point. This can be any valid intersection.
+    # In ed2pl[i][0], the 0 could also be a one. It would mean that we'd
+    # move around the box in the other direction.
     plane_index = 0
     for i in range(12):
-        if intersect_flag[i] == 1:
+        if intersect_flags[i] == 1:
             plane_index = ed2pl[i][0]
-            vertices[0] = intersect_pos[i]
-            # break
+            vertices[0] = intersect_positions[i]
+            texcoords[0] = intersect_texcoords[i]
+            break
 
     # From there take (at most) 5 steps
     i_start = i
     i_last = i
-    max_iter = 5
+    max_iter = 6
     for iter in range(1, max_iter):
         for i in range(12):
-            if i != i_last and intersect_flag[i] == 1:
+            if i != i_last and intersect_flags[i] == 1:
                 if ed2pl[i][0] == plane_index:
-                    vertices[iter] = intersect_pos[i]
+                    vertices[iter] = intersect_positions[i]
+                    texcoords[iter] = intersect_texcoords[i]
                     plane_index = ed2pl[i][1]
                     i_last = i
-                    break  # i = 99   # break
+                    break
                 elif ed2pl[i][1] == plane_index:
-                    vertices[iter] = intersect_pos[i]
+                    vertices[iter] = intersect_positions[i]
+                    texcoords[iter] = intersect_texcoords[i]
                     plane_index = ed2pl[i][0]
                     i_last = i
-                    break  # i = 99  # break
+                    break
         if i_last == i_start:
             max_iter = iter
-            break  # - can't
+            break
 
     # Make the rest degenerate triangles
-    n_intersections = max_iter + 1
+    n_intersections = max_iter
     for i in range(max_iter, 6):
         vertices[i] = vertices[0]
 
-    ##
-    # Now select the current vertex. We mimic a triangle fan with a triangle list
+    # Now select the current vertex. We mimic a triangle fan with a triangle list.
+    # This works whether the number of vertices/intersections is 3, 4, 5, and 6.
     indexmap = [0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 5]
     the_pos = vertices[indexmap[index]]
+    the_tc = texcoords[indexmap[index]]
 
     world_pos = u_wobject.world_transform * vec4(the_pos.xyz, 1.0)
     ndc_pos = u_stdinfo.projection_transform * u_stdinfo.cam_transform * world_pos
     out_pos = ndc_pos  # noqa - shader output
+    v_texcoord = the_tc  # noqa - shader output
 
 
 @python2shader
 def fragment_shader_textured_gray(
     v_texcoord: (pyshader.RES_INPUT, 0, vec3),
     u_material: (pyshader.RES_UNIFORM, (0, 2), VolumeSliceMaterial.uniform_type),
-    s_sam: (pyshader.RES_SAMPLER, (1, 0), ""),
-    t_tex: (pyshader.RES_TEXTURE, (1, 1), "3d i32"),
+    s_sam: (pyshader.RES_SAMPLER, (1, 2), ""),
+    t_tex: (pyshader.RES_TEXTURE, (1, 3), "3d i32"),
     out_color: (pyshader.RES_OUTPUT, 0, vec4),
 ):
     val = f32(t_tex.sample(s_sam, v_texcoord).r)
     val = (val - u_material.clim[0]) / (u_material.clim[1] - u_material.clim[0])
-    val = 255
     out_color = vec4(val, val, val, 1.0)  # noqa - shader output
