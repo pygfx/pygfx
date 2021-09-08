f6fdb3880a8b86e9f7fa1b4e1ccfb477abb38cb8
Almar Klein <almar@almarklein.org>
2021-09-08 10:33:22 +0200

    Implement test to ensure material inheritance makes sense.

---
+++ tests/objects/test_materials.py
@@ -0,0 +1,30 @@
+import pygfx
+
+
+# Collect materials
+material_classes = []
+for name in dir(pygfx.materials):
+    val = getattr(pygfx.materials, name)
+    if isinstance(val, type) and issubclass(val, pygfx.Material):
+        material_classes.append(val)
+
+
+def test_uniform_types_uniform_type():
+    """Check that the uniform_type always includes that of the super."""
+    for cls in material_classes:
+        assert isinstance(cls.uniform_type, dict)
+        for super_cls in cls.mro():
+            if super_cls is cls:
+                continue
+            elif not hasattr(super_cls, "uniform_type"):
+                break
+            else:
+                for key, val in super_cls.uniform_type.items():
+                    assert key in cls.uniform_type, f"{cls.__name__}:{key} missing"
+                    assert (
+                        val == cls.uniform_type[key]
+                    ), f"{cls.__name__}:{key} different"
+
+
+if __name__ == "__main__":
+    test_uniform_types_uniform_type()
