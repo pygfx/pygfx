2861795ee6c8b62c713b5741b052d9784297100d
Almar Klein <almar@almarklein.org>
2021-09-15 22:09:50 +0200

    Example flat shading

---
+++ examples/flat_shaded_torus.py
@@ -0,0 +1,39 @@
+"""
+Example showing a Torus knot, using flat shading.
+"""
+
+import pygfx as gfx
+
+from PyQt5 import QtWidgets
+from wgpu.gui.qt import WgpuCanvas
+
+
+app = QtWidgets.QApplication([])
+
+canvas = WgpuCanvas()
+renderer = gfx.renderers.WgpuRenderer(canvas)
+scene = gfx.Scene()
+
+
+geometry = gfx.TorusKnotGeometry(1, 0.3, 64, 10)
+geometry.texcoords.data[:, 0] *= 10  # stretch the texture
+material = gfx.MeshFlatMaterial(color=(1, 0, 1, 1))
+obj = gfx.Mesh(geometry, material)
+scene.add(obj)
+
+
+camera = gfx.PerspectiveCamera(70, 1)
+camera.position.z = 4
+
+
+def animate():
+    rot = gfx.linalg.Quaternion().set_from_euler(gfx.linalg.Euler(0.0071, 0.01))
+    obj.rotation.multiply(rot)
+
+    renderer.render(scene, camera)
+    canvas.request_draw()
+
+
+if __name__ == "__main__":
+    canvas.request_draw(animate)
+    app.exec_()
