e270f42f1da5104d1fd2ea268a8833b70ff5e6a9
Almar Klein <almar@almarklein.org>
2021-10-14 22:37:18 +0200

    add example

---
+++ examples/volume_render.py
@@ -0,0 +1,75 @@
+"""
+Render a volume.
+"""
+
+import imageio
+import pygfx as gfx
+
+from PyQt5 import QtWidgets, QtCore
+from wgpu.gui.qt import WgpuCanvas
+
+
+class WgpuCanvasWithInputEvents(WgpuCanvas):
+    _drag_modes = {QtCore.Qt.RightButton: "pan", QtCore.Qt.LeftButton: "rotate"}
+    _mode = None
+
+    def wheelEvent(self, event):  # noqa: N802
+        controls.zoom(2 ** (event.angleDelta().y() * 0.0015))
+
+    def mousePressEvent(self, event):  # noqa: N802
+        mode = self._drag_modes.get(event.button(), None)
+        if self._mode or not mode:
+            return
+        self._mode = mode
+        drag_start = (
+            controls.pan_start if self._mode == "pan" else controls.rotate_start
+        )
+        drag_start(
+            (event.x(), event.y()),
+            self.get_logical_size(),
+            camera,
+        )
+        app.setOverrideCursor(QtCore.Qt.ClosedHandCursor)
+
+    def mouseReleaseEvent(self, event):  # noqa: N802
+        if self._mode and self._mode == self._drag_modes.get(event.button(), None):
+            self._mode = None
+            drag_stop = (
+                controls.pan_stop if self._mode == "pan" else controls.rotate_stop
+            )
+            drag_stop()
+            app.restoreOverrideCursor()
+
+    def mouseMoveEvent(self, event):  # noqa: N802
+        if self._mode is not None:
+            drag_move = (
+                controls.pan_move if self._mode == "pan" else controls.rotate_move
+            )
+            drag_move((event.x(), event.y()))
+
+
+app = QtWidgets.QApplication([])
+canvas = WgpuCanvasWithInputEvents()
+renderer = gfx.renderers.WgpuRenderer(canvas)
+scene = gfx.Scene()
+
+voldata = imageio.volread("imageio:stent.npz")
+
+vol = gfx.Volume(voldata, gfx.VolumeRayMaterial(clim=(0, 2000)))
+scene.add(vol)
+
+camera = gfx.PerspectiveCamera(70, 16 / 9)
+camera.position.z = 500
+controls = gfx.OrbitControls(camera.position.clone())
+
+
+def animate():
+    controls.update_camera(camera)
+
+    renderer.render(scene, camera)
+    canvas.request_draw()
+
+
+if __name__ == "__main__":
+    canvas.request_draw(animate)
+    app.exec()
