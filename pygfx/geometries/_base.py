fix world bounding box computation

pygfx/geometries/_base.py
@@ -95,19 +95,24 @@ class Geometry(ResourceContainer):
             setattr(self, name, resource)
 
     def bounding_box(self):
-        if self._aabb_rev == self.positions.rev:
-            return self._aabb
-
         if not hasattr(self, "positions"):
             raise ValueError(
                 "No position buffer available for bounding box computation"
             )
+
+        if self._aabb_rev == self.positions.rev:
+            return self._aabb
         pos = self.positions.data
         self._aabb = np.array([pos.min(axis=0), pos.max(axis=0)])
         self._aabb_rev = self.positions.rev
         return self._aabb
 
     def bounding_sphere(self):
+        if not hasattr(self, "positions"):
+            raise ValueError(
+                "No position buffer available for bounding box computation"
+            )
+
         if self._bsphere_rev == self.positions.rev:
             return self._bsphere
 

pygfx/objects/_base.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 import weakref
 import threading
@@ -410,13 +411,14 @@ class WorldObject(ResourceContainer):
     def get_world_bounding_box(self):
         self.update_matrix_world(update_parents=True, update_children=False)
         bbox = self._geometry.bounding_box()
-        transformed = np.array(
+        corners = np.array(list(itertools.product(*bbox.T)))
+        corners_world = np.array(
             [
-                Vector3(*bbox[0]).apply_matrix4(self._matrix_world).to_array(),
-                Vector3(*bbox[1]).apply_matrix4(self._matrix_world).to_array(),
+                Vector3(*corner).apply_matrix4(self._matrix_world).to_array()
+                for corner in corners
             ]
         )
-        bbox_world = np.array([transformed.min(axis=0), transformed.max(axis=0)])
+        bbox_world = np.array([corners_world.min(axis=0), corners_world.max(axis=0)])
         return bbox_world
 
     def get_world_bounding_sphere(self):