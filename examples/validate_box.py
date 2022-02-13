b7a5e51872a2e0a274ebee39150a5f84d122d5fc
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-02-13 21:28:46 +0100

    Fix box geometry (#247)

---
+++ examples/validate_box.py
@@ -0,0 +1,31 @@
+"""
+Example showing the box geometry.
+"""
+# test_example = true
+
+from wgpu.gui.auto import WgpuCanvas, run
+import pygfx as gfx
+
+canvas = WgpuCanvas()
+renderer = gfx.renderers.WgpuRenderer(canvas)
+
+scene = gfx.Scene()
+
+material = gfx.MeshBasicMaterial(color=(1, 0, 0), wireframe=True)
+geometry = gfx.box_geometry(1, 2, 3)
+box = gfx.Mesh(geometry, material)
+scene.add(box)
+
+material2 = gfx.MeshNormalLinesMaterial(color=(0, 1, 0))
+box2 = gfx.Mesh(geometry, material2)
+scene.add(box2)
+
+camera = gfx.OrthographicCamera(5, 5)
+camera.position.set(5, 5, 5)
+camera.look_at(gfx.linalg.Vector3())
+
+canvas.request_draw(lambda: renderer.render(scene, camera))
+
+if __name__ == "__main__":
+    print(__doc__)
+    run()
