fc64a70c68c44a24bfda96563fa56087e20a59c9
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-03-16 11:51:22 +0100

    Fix bug in deciding whether to add pick info

--- pygfx/renderers/wgpu/_renderer.py
+++ pygfx/renderers/wgpu/_renderer.py
@@ -743,7 +743,7 @@ class WgpuRenderer(RootEventHandler, Renderer):
         event_type = event["event_type"]
         target = None
         info = None
-        if "x" in event or "y" in event:
+        if "x" in event and "y" in event:
             info = self.get_pick_info((event["x"], event["y"]))
             target = info["world_object"]
 
