73cb001649df503ca21cca3216e1a14a3cd95f9e
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-10-14 16:42:07 +0200

    make gltf loader a little bit more generic

--- pygfx/utils/gltf.py
+++ pygfx/utils/gltf.py
@@ -28,6 +28,7 @@ def load_gltf(path):
 
 def _parse_mesh(mesh):
     visual = mesh.visual
+    # todo: why?
     visual.uv = visual.uv * np.array([1, -1]) + np.array([0, 1])  # uv.y = 1 - uv.y
     return gfx.Mesh(
         _parse_geometry(mesh),
@@ -41,22 +42,34 @@ def _parse_geometry(mesh):
 
 def _parse_material(pbrmaterial):
     material = gfx.MeshStandardMaterial()
-    material.map = _parse_texture(pbrmaterial.baseColorTexture)
 
-    material.emissive = gfx.Color(*pbrmaterial.emissiveFactor)
-    material.emissive_map = _parse_texture(pbrmaterial.emissiveTexture)
+    if pbrmaterial.baseColorTexture is not None:
+        material.map = _parse_texture(pbrmaterial.baseColorTexture)
 
-    metallic_roughness_map = _parse_texture(pbrmaterial.metallicRoughnessTexture)
-    material.roughness = pbrmaterial.roughnessFactor or 1.0
-    material.metalness = pbrmaterial.metallicFactor or 1.0
-    material.roughness_map = metallic_roughness_map
-    material.metalness_map = metallic_roughness_map
+    if pbrmaterial.emissiveFactor is not None:
+        material.emissive = gfx.Color(*pbrmaterial.emissiveFactor)
+    
+    if pbrmaterial.emissiveTexture is not None:
+        material.emissive_map = _parse_texture(pbrmaterial.emissiveTexture)
 
-    material.normal_map = _parse_texture(pbrmaterial.normalTexture)
-    material.normal_scale = (1, -1)
+    if pbrmaterial.metallicRoughnessTexture is not None:
+        metallic_roughness_map = _parse_texture(pbrmaterial.metallicRoughnessTexture)
+        material.roughness_map = metallic_roughness_map
+        material.metalness_map = metallic_roughness_map
 
-    material.ao_map = _parse_texture(pbrmaterial.occlusionTexture)
-    material.ao_map_intensity = 1.0
+    if pbrmaterial.roughnessFactor is not None:
+        material.roughness = pbrmaterial.roughnessFactor
+    
+    if pbrmaterial.metallicFactor is not None:
+        material.metalness = pbrmaterial.metallicFactor
+
+    if pbrmaterial.normalTexture is not None:
+        material.normal_map = _parse_texture(pbrmaterial.normalTexture)
+        # todo: why is this different from the default for MeshStandardMaterial?
+        material.normal_scale = (1, -1)
+
+    if pbrmaterial.occlusionTexture is not None:
+        material.ao_map = _parse_texture(pbrmaterial.occlusionTexture)
 
     material.side = "FRONT"
     return material
@@ -66,6 +79,10 @@ def _parse_texture(pil_image):
     if pil_image is None:
         return None
     m = memoryview(pil_image.tobytes())
-    m = m.cast(m.format, shape=(pil_image.size[0], pil_image.size[1], 3))
+
+    im_channels = len(pil_image.getbands())
+    buffer_shape = pil_image.size + (im_channels,)
+
+    m = m.cast(m.format, shape=buffer_shape)
     tex = gfx.Texture(m, dim=2)
     return tex.get_view(address_mode="repeat", filter="linear")
