027781da0edfd74396e5327fb389ae6d02cf0725
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-10-26 09:45:08 +0200

    add validation example for light and shadow

--- examples/validate_light_shadow.py
+++ examples/validate_light_shadow.py
@@ -1,5 +1,7 @@
 """
-This example combines all types of lights and all types of materials with shadows.
+This example combines MeshPhongMaterial and MeshStandardMaterial with
+PointLight, AmbientLight, SpotLight and DirectionalLight to check
+that all combinations are working properly.
 """
 # test_example = true
 
@@ -34,7 +36,6 @@ cube3 = gfx.Mesh(
     gfx.box_geometry(100, 100, 100),
     gfx.MeshPhongMaterial(),
 )
-
 cube3.position.set(0, -250, 0)
 cube3.cast_shadow = True
 cube3.receive_shadow = True
@@ -44,7 +45,6 @@ box = gfx.Mesh(
     gfx.box_geometry(600, 600, 600),
     gfx.MeshStandardMaterial(color="#808080", side="Back"),
 )
-
 box.rotation.set_from_euler(gfx.linalg.Euler(-math.pi / 2))
 box.position.set(0, 0, 0)
 box.receive_shadow = True
@@ -52,13 +52,11 @@ box.cast_shadow = False
 scene.add(box)
 
 ambient = gfx.AmbientLight()
-
 scene.add(ambient)
 
 light = gfx.PointLight("#4040ff", 500000, decay=2)
 light.position.x = 15
 light.position.y = 20
-
 light.cast_shadow = True
 scene.add(light)
 
@@ -67,8 +65,14 @@ light2.position.x = -150
 light2.position.y = 100
 light2.position.z = 100
 light2.cast_shadow = True
-
 scene.add(light2)
+
+light3 = gfx.SpotLight("#ffffff", 500, angle=0.3, penumbra=0.2, decay=1)
+light3.position.x = 0
+light3.position.y = 0
+light3.position.z = 100
+light3.cast_shadow = True
+scene.add(light3)
 
 camera = gfx.PerspectiveCamera(70, 16 / 9)
 camera.position.x = 0
