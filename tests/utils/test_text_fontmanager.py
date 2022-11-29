7dc629e5771c7b812dac9f9e646a4514ffaa4bda
Almar Klein <almar@almarklein.org>
2022-11-29 11:45:00 +0100

    Add tests for text-steps, including time measurements to identify bottlenecks

---
+++ tests/utils/test_text_fontmanager.py
@@ -0,0 +1,51 @@
+import time
+
+from pygfx.utils.text import font_manager, FontInfo
+
+
+def test_select_font():
+
+    # A simple text, that can be rendered with the main font
+    text = "HelloWorld"
+
+    pieces = font_manager.select_font(text, ())
+    assert len(pieces) == 1
+    assert isinstance(pieces[0], tuple)
+    assert pieces[0][0] == text
+    assert isinstance(pieces[0][1], FontInfo)
+
+    # A text with both Latin and Arabic, needs two fonts
+    text = "Hello World مرحبا بالعالم"
+
+    pieces = font_manager.select_font(text, ())
+    assert len(pieces) == 2
+    assert isinstance(pieces[0], tuple)
+    assert isinstance(pieces[1], tuple)
+    assert isinstance(pieces[0][1], FontInfo)
+    assert isinstance(pieces[1][1], FontInfo)
+    assert pieces[0][0] == "Hello World "
+    assert pieces[1][0] == "مرحبا بالعالم"
+
+
+def check_speed():
+    text = "HelloWorld"
+
+    t0 = time.perf_counter()
+    for i in range(1000):
+        font_manager.select_font(text, ())
+    dt = time.perf_counter() - t0
+    print(
+        f"select_font: {1000*dt:0.1f} ms total", f"{1000*dt/(10000):0.3f} ms per char"
+    )
+
+    # About 0.00 ms (0.3 us), so this  negligible.
+
+
+if __name__ == "__main__":
+    for ob in list(globals().values()):
+        if callable(ob) and ob.__name__.startswith("test_"):
+            print(f"{ob.__name__} ...")
+            ob()
+    print("done")
+
+    check_speed()
---
+++ tests/utils/test_text_shaping.py
@@ -0,0 +1,189 @@
+import os
+import time
+
+import numpy as np
+from pygfx.utils.text._shaper import TemperalCache, shape_text_hb, shape_text_ft
+from pygfx.utils.text import font_manager
+
+
+def test_cache():
+    c = TemperalCache(0.1)
+
+    # === Store one value
+    c.set("foo", 42)
+    assert c.get("foo") == 42
+    assert c.get("foo") == 42
+    assert len(c._cache) == 1
+
+    # Wait for the entry to become old.
+    # But because we get the item, it activates again
+    time.sleep(0.11)
+    assert c.get("foo") == 42
+    assert c.get("foo") == 42
+
+    # Wait again, now get a nonexistent item to trigger removing foo
+    time.sleep(0.11)
+    assert c.get("bar") is None
+    assert c.get("foo") is None
+    assert len(c._cache) == 0
+
+    # === Again, but with two
+    c.set("foo", 42)
+    c.set("bar", 90)
+    assert c.get("foo") == 42
+    assert c.get("bar") == 90
+    assert len(c._cache) == 2
+
+    # Wait for the entry to become old.
+    # The first one we get() is valid, but the get triggers a cleanup.
+    time.sleep(0.11)
+    assert c.get("foo") == 42
+    assert c.get("bar") is None
+    assert len(c._cache) == 1
+
+    # Wait again, now get a nonexistent item to trigger removing foo
+    time.sleep(0.11)
+    assert c.get("bar") is None
+    assert c.get("foo") is None
+    assert len(c._cache) == 0
+
+    # === Put a lot of items in there
+    for i in range(1000):
+        c.set(i, i)
+    assert len(c._cache) == 1000
+
+    for i in range(1000):
+        assert c.get(i) == i
+
+    time.sleep(1.1)
+    assert c.get(0) == 0
+    for i in range(1, 1000):
+        assert c.get(i) is None
+    assert len(c._cache) == 1
+
+
+def test_shape_text_hb():
+    font = font_manager.select_fonts_for_codepoint(32, ())[0]
+    text = "HelloWorld"
+
+    result = shape_text_hb(text, font.filename)
+    check_result(*result)
+
+
+def test_shape_text_ft():
+    font = font_manager.select_fonts_for_codepoint(32, ())[0]
+    text = "HelloWorld"
+
+    result = shape_text_ft(text, font.filename)
+    check_result(*result)
+
+
+def check_result(indices, positions, meta):
+    assert isinstance(indices, np.ndarray)
+    assert indices.dtype == np.uint32 and indices.ndim == 1
+
+    assert isinstance(positions, np.ndarray)
+    assert (
+        positions.dtype == np.float32
+        and positions.ndim == 2
+        and positions.shape[1] == 2
+    )
+
+    assert isinstance(meta, dict)
+    assert "width" in meta.keys()
+    assert meta["width"] > positions[:, 0].max()
+    assert "direction" in meta.keys()
+    assert "script" in meta.keys()
+
+
+def test_glyph_size():
+    font = font_manager.select_fonts_for_codepoint(32, ())[0]
+
+    _, positions1, meta1 = shape_text_hb("iiii", font.filename)
+    _, positions2, meta2 = shape_text_hb("MMMM", font.filename)
+
+    xx1 = positions1[1:, 0]
+    xx2 = positions2[1:, 0]
+    assert np.all(xx1 < xx2)
+
+    assert meta2["width"] > meta1["width"]
+
+
+def check_speed():
+    font = font_manager.select_fonts_for_codepoint(32, ())[0]
+    text = "HelloWorld"
+
+    t0 = time.perf_counter()
+    for i in range(1000):
+        shape_text_hb(text, font.filename)
+    dt = time.perf_counter() - t0
+    print(
+        f"shape_text_hb: {1000*dt:0.1f} ms total",
+        f"{1000*dt/(10000):0.3f} ms per glyph",
+    )
+
+    t0 = time.perf_counter()
+    for i in range(1000):
+        shape_text_ft(text, font.filename)
+    dt = time.perf_counter() - t0
+    print(
+        f"shape_text_ft: {1000*dt:0.1f} ms total",
+        f"{1000*dt/(10000):0.3f} ms per glyph",
+    )
+
+    # No cache:    about 0.03  and 0.02  ms per glyph for Harfbuzz and FreeType, respectively.
+    # Witch cache: about 0.001 and 0.003 ms per glyph for Harfbuzz and FreeType, respectively.
+
+
+def check_mem_to_store_face_objects():
+    """Function to test how much memory it costs to store a FreeType
+    or HarfBuzz font object. Intended to be run locally, for reference.
+
+    Results:
+    * For FT, about 700 KB for NotoSansSC-Regular.otf
+    * For FT, about  37 KB for NotoSans-Regular.ttf
+    * For HB, about 16 KB for NotoSansSC-Regular.otf
+    * For HB, about 16 KB for NotoSans-Regular.ttf
+    """
+    import psutil, freetype, uharfbuzz  # noqa
+
+    font = font_manager.select_fonts_for_codepoint(32, ())[0]
+    font_filename = font.filename
+    # font_filename = "/Users/almar/dev/py/pygfx/pygfx/pkg_resources/fonts/noto_cjk_man/NotoSansSC-Regular.otf"
+
+    m0 = psutil.Process(os.getpid()).memory_info().rss / 1024
+    ft_faces = []
+
+    for j in range(10):
+        for i in range(100):
+            face = freetype.Face(font_filename)
+            face.set_pixel_sizes(48, 48)
+            ft_faces.append(face)
+        m1 = psutil.Process(os.getpid()).memory_info().rss / 1024
+        dm = (m1 - m0) / len(ft_faces)
+        print(f"{dm:0.0f} KiB per ft face")
+
+    m0 = psutil.Process(os.getpid()).memory_info().rss / 1024
+    hb_faces = []
+
+    for j in range(10):
+        for i in range(100):
+            blob = uharfbuzz.Blob.from_file_path(font_filename)
+            face = uharfbuzz.Face(blob)
+            font = uharfbuzz.Font(face)
+            font.scale = 48, 48
+            hb_faces.append((blob, face, font))
+        m1 = psutil.Process(os.getpid()).memory_info().rss / 1024
+        dm = (m1 - m0) / len(hb_faces)
+        print(f"{dm:0.0f} KiB per hb face")
+
+
+if __name__ == "__main__":
+    for ob in list(globals().values()):
+        if callable(ob) and ob.__name__.startswith("test_"):
+            print(f"{ob.__name__} ...")
+            ob()
+    print("done")
+
+    check_speed()
+    # check_mem_to_store_face_objects()
