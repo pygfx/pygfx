755d5ded7348e70c7ac240c53bb9571b72d8261f
Sebastian Wallkötter <sebastian@wallkoetter.net>
2022-11-30 07:13:47 +0100

    xfail if wgpu version is too low

--- tests/utils/test_display.py
+++ tests/utils/test_display.py
@@ -1,10 +1,15 @@
-import pygfx as gfx
 import pytest
+import wgpu
 from wgpu.gui.offscreen import WgpuCanvas
+
+import pygfx as gfx
 
 
 def test_repeated_show():
-    pytest.xfail("Offscreen canvas can currently not be closed.")
+    wgpu_version = tuple(int(x) for x in wgpu.__version__.split("."))
+
+    if wgpu_version <= (0, 8, 2):
+        pytest.xfail("Offscreen canvas can not be closed.")
 
     cube = gfx.Mesh(
         gfx.box_geometry(200, 200, 200),
