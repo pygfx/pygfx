bfa418f1dc04c313d875fc033c645e8d74ef51e0
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-12-07 15:32:34 +0100

    clean up the diff

--- examples/tests/test_examples.py
+++ examples/tests/test_examples.py
@@ -97,50 +97,53 @@ def mock_time():
 
 @pytest.mark.parametrize("module", examples_to_test)
 def test_examples_screenshots(
-    module, pytestconfig, force_offscreen, disable_call_later_after_run, mock_time
+    module, pytestconfig, force_offscreen, mock_time, request
 ):
     """Run every example marked for testing."""
 
-    # render
+    # import the example module
     example = importlib.import_module(f"examples.{module}")
 
-    try:
-        img = example.renderer.target.draw()
-
-        # check if _something_ was rendered
-        assert img is not None and img.size > 0
-
-        # we skip the rest of the test if you are not using lavapipe
-        # images come out subtly differently when using different wgpu adapters
-        # so for now we only compare screenshots generated with the same adapter (lavapipe)
-        # a benefit of using pytest.skip is that you are still running
-        # the first part of the test everywhere else; ensuring that examples
-        # can at least import, run and render something
-        if not is_lavapipe:
-            pytest.skip("screenshot comparisons are only done when using lavapipe")
-
-        # regenerate screenshot if requested
-        screenshot_path = screenshots_dir / f"{module}.png"
-        if pytestconfig.getoption("regenerate_screenshots"):
-            iio.imwrite(screenshot_path, img)
-
-        # if a reference screenshot exists, assert it is equal
-        assert (
-            screenshot_path.exists()
-        ), "found # test_example = true but no reference screenshot available"
-        stored_img = iio.imread(screenshot_path)
-        # assert similarity
-        is_similar = np.allclose(img, stored_img, atol=1)
-        update_diffs(module, is_similar, img, stored_img)
-        assert is_similar, (
-            f"rendered image for example {module} changed, see "
-            f"the {diffs_dir.relative_to(ROOT).as_posix()} folder"
-            " for visual diffs (you can download this folder from"
-            " CI build artifacts as well)"
-        )
-    finally:
-        # ensure the imported module is unloaded
+    # ensure it is unloaded after the test
+    def unload_module():
         del sys.modules[f"examples.{module}"]
+
+    request.addfinalizer(unload_module)
+
+    # render a frame
+    img = example.renderer.target.draw()
+
+    # check if _something_ was rendered
+    assert img is not None and img.size > 0
+
+    # we skip the rest of the test if you are not using lavapipe
+    # images come out subtly differently when using different wgpu adapters
+    # so for now we only compare screenshots generated with the same adapter (lavapipe)
+    # a benefit of using pytest.skip is that you are still running
+    # the first part of the test everywhere else; ensuring that examples
+    # can at least import, run and render something
+    if not is_lavapipe:
+        pytest.skip("screenshot comparisons are only done when using lavapipe")
+
+    # regenerate screenshot if requested
+    screenshot_path = screenshots_dir / f"{module}.png"
+    if pytestconfig.getoption("regenerate_screenshots"):
+        iio.imwrite(screenshot_path, img)
+
+    # if a reference screenshot exists, assert it is equal
+    assert (
+        screenshot_path.exists()
+    ), "found # test_example = true but no reference screenshot available"
+    stored_img = iio.imread(screenshot_path)
+    # assert similarity
+    is_similar = np.allclose(img, stored_img, atol=1)
+    update_diffs(module, is_similar, img, stored_img)
+    assert is_similar, (
+        f"rendered image for example {module} changed, see "
+        f"the {diffs_dir.relative_to(ROOT).as_posix()} folder"
+        " for visual diffs (you can download this folder from"
+        " CI build artifacts as well)"
+    )
 
 
 def update_diffs(module, is_similar, img, stored_img):
