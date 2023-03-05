479c2dd2e1425b98b4e3a530809ed391e4e6295a
Kushal Kolar <kushalkolar@gmail.com>
2023-03-05 18:20:50 -0500

    uint32 is "u4" not "i4"
    Co-authored-by: Almar Klein <almar@almarklein.org>

--- pygfx/resources/_texture.py
+++ pygfx/resources/_texture.py
@@ -248,8 +248,7 @@ def format_from_memoryview(mem, size):
         "h": "i2",
         "H": "u2",
         "i": "i4",
-        "I": "i4",
-        "U": "u4",
+        "I": "u4",
         "e": "f2",
         "f": "f4",
     }
