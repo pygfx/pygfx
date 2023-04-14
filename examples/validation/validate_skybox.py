a630b08e7541cf3d60cde40a3beb60adce57c0f2
Almar Klein <almar@almarklein.org>
2023-04-14 13:14:46 +0200

    Add validation example for skybox and env map

---
+++ examples/validation/validate_skybox.py
@@ -0,0 +1,61 @@
+"""
+Validate Skybox
+===============
+
+This validates that the skybox background is rendered correctly,
+as well as that the environment-map matches that background. Therefore
+it also covers the internal cube-camera-renderer.
+
+Note that the blue -Z side is marked "back", but since camera's look down the
+-Z direction, it'd be what the camera would see if it was not rotated.
+"""
+
+import numpy as np
+import imageio
+import pygfx as gfx
+from pathlib import Path
+from wgpu.gui.auto import WgpuCanvas, run
+
+
+env_map_path = Path(__file__).parents[1] / "data" / "cubemap.jpg"
+data = imageio.imread(Path(env_map_path))
+
+h = data.shape[0] // 3
+w = data.shape[1] // 4
+
+posx = np.ascontiguousarray(data[1 * h : 2 * h, 2 * w : 3 * w])
+negx = np.ascontiguousarray(data[1 * h : 2 * h, 0 * w : 1 * w])
+posy = np.ascontiguousarray(data[0 * h : 1 * h, 1 * w : 2 * w])
+negy = np.ascontiguousarray(data[2 * h : 3 * h, 1 * w : 2 * w])
+posz = np.ascontiguousarray(data[1 * h : 2 * h, 1 * w : 2 * w])
+negz = np.ascontiguousarray(data[1 * h : 2 * h, 3 * w : 4 * w])
+
+datas = [posx, negx, posy, negy, posz, negz]
+
+tex = gfx.Texture(np.stack(datas, axis=0), dim=2, size=(w, h, 6))
+
+canvas = WgpuCanvas(size=(640, 640))
+renderer = gfx.renderers.WgpuRenderer(canvas)
+
+scene = gfx.Scene()
+
+background = gfx.Background(None, gfx.BackgroundSkyboxMaterial(map=tex))
+scene.add(background)
+
+mesh = gfx.Mesh(
+    gfx.sphere_geometry(2, 64, 64),
+    gfx.MeshStandardMaterial(roughness=0.01, metalness=1, side="Front"),
+)
+mesh.material.env_map = tex
+scene.add(mesh)
+
+camera = gfx.PerspectiveCamera(90)
+camera.position.set(2, 3, 4)
+camera.show_pos(mesh)
+
+controller = gfx.OrbitController(camera, register_events=renderer)
+
+
+if __name__ == "__main__":
+    renderer.request_draw(lambda: renderer.render(scene, camera))
+    run()
