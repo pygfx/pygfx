ede3f76d41361194e96b41c2f34b8a8c18881b4d
Sebastian Wallkötter <sebastian@wallkoetter.net>
2023-04-14 09:55:50 +0200

    better assertion
    Co-authored-by: Almar Klein <almar@almarklein.org>

--- tests/objects/test_world_object.py
+++ tests/objects/test_world_object.py
@@ -160,7 +160,7 @@ def test_adjust_children_order():
     expected_children = (child3, child1, child2)
     assert len(root.children) == len(expected_children)
     for actual, expected in zip(root.children, expected_children):
-        assert actual == expected
+        assert actual is expected
 
 
 def test_iter():
