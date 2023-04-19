27c7cc43486d49a78f3bc7d4210c5a11977af54e
kushalkolar <kushalkolar@gmail.com>
2023-04-19 02:55:32 -0400

    add example of how to map screen to world

---
+++ examples/feature_demo/map_click_events_to_world.py
@@ -0,0 +1,128 @@
+"""
+Map Screen to World
+===================
+
+This shows how to map from the screen to the world
+by adding scatter points at click events locations
+"""
+# sphinx_gallery_pygfx_render = True
+
+from wgpu.gui.auto import WgpuCanvas, run
+
+import pygfx
+import pygfx as gfx
+from pygfx.linalg import Vector3
+import numpy as np
+
+canvas = WgpuCanvas()
+renderer = gfx.renderers.WgpuRenderer(canvas)
+
+# default cam position
+center_cam_pos = (256, 256, 0)
+
+
+# lists of everything necessary to make this plot
+scenes = list()
+cameras = list()
+controllers = list()
+camera_defaults = list()
+viewports = list()
+
+bg_colors = ["r", "g", "b", "y"]
+
+for i in range(4):
+    # create scene for this subplot
+    scene = gfx.Scene()
+    scenes.append(scene)
+
+    bg = gfx.Background(None, gfx.BackgroundMaterial(bg_colors[i]))
+    scene.add(bg)
+
+    # create camera, set default position, add to list
+    camera = gfx.OrthographicCamera(512, 512)
+    camera.show_rect(0, 512, 0, 512)
+    cameras.append(camera)
+
+    # create viewport
+    viewport = gfx.Viewport(renderer)
+    viewports.append(viewport)
+
+    # controller for pan & zoom
+    controller = gfx.PanZoomController(camera, register_events=renderer)
+    controllers.append(controller)
+
+    # get the initial controller params so the camera can be reset later
+    camera_defaults.append(camera.get_state())
+
+
+@renderer.add_event_handler("resize")
+def layout(event=None):
+    """
+    Update the viewports when the canvas is resized
+    """
+    w, h = renderer.logical_size
+    w2, h2 = w / 2, h / 2
+    viewports[0].rect = 10, 10, w2, h2
+    viewports[1].rect = w / 2 + 5, 10, w2, h2
+    viewports[2].rect = 10, h / 2 + 5, w2, h2
+    viewports[3].rect = w / 2 + 5, h / 2 + 5, w2, h2
+
+
+def animate():
+    # render the viewports
+    for viewport, s, c in zip(viewports, scenes, cameras):
+        viewport.render(s, c)
+
+    renderer.flush()
+    canvas.request_draw()
+
+
+def add_point(ev):
+    """"Add point at mouse click location by click position in renderer space to world space"""
+
+    # this is the click position in renderer screen space
+    pos = (ev.x, ev.y)
+    print(f"position click: {pos}")
+
+    for viewport, scene, camera in zip(viewports, scenes, cameras):
+        if not viewport.is_inside(*pos):
+            continue
+
+        # get position relative to viewport
+        pos_rel = (
+            pos[0] - viewport.rect[0],
+            pos[1] - viewport.rect[1],
+        )
+
+        vs = viewport.logical_size
+
+        # convert position to NDC
+        pos_ndc = Vector3(
+            pos_rel[0] / vs[0] * 2 - 1,
+            -(pos_rel[1] / vs[1] * 2 - 1),
+            0
+        )
+
+        # get world position
+        pos_world = camera.position.clone().project(camera).add(pos_ndc).unproject(camera)
+        print(f"position world: {pos_world}")
+
+        # make point data
+        point_data = np.array([[pos_world.x, pos_world.y, pos_world.z]], dtype=np.float32)
+
+        # add point
+        point = gfx.Points(
+            gfx.Geometry(positions=point_data, sizes=[10], colors=[pygfx.Color("black")]),
+            material=gfx.PointsMaterial(vertex_colors=True, vertex_sizes=True)
+        )
+
+        scene.add(point)
+
+
+renderer.add_event_handler(add_point, "click")
+layout()
+
+
+if __name__ == "__main__":
+    canvas.request_draw(animate)
+    run()
