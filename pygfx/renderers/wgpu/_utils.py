4aa4917f68e8a315ccb7dc1a02a79d3a75df67a4
Almar Klein <almar@almarklein.org>
2023-04-21 11:43:18 +0200

    Introduce new cache object

--- pygfx/renderers/wgpu/_utils.py
+++ pygfx/renderers/wgpu/_utils.py
@@ -2,6 +2,8 @@
 Utils for the wgpu renderer.
 """
 
+import weakref
+
 import wgpu
 
 from .._base import RenderFunctionRegistry
@@ -191,6 +193,37 @@ def generate_uniform_struct(dtype_struct, structname):
     code += "\n        };"
 
     return code
+
+
+class GpuCache:
+    """A chache for GPU objects."""
+
+    _caches = {}
+
+    @classmethod
+    def get_cache_stats(cls):
+        return {name: cache.get_count() for name, cache in GpuCache._caches.items()}
+
+    def __init__(self, name):
+        assert isinstance(name, str)
+        assert name not in GpuCache._caches
+        GpuCache._caches[name] = self
+
+        self._objects = weakref.WeakValueDictionary()
+
+    def get_count(self):
+        """Get the number of (alive) objects in the cache."""
+        return len(list(self._objects.values()))
+
+    def get(self, hash):
+        """Get the cached object or None."""
+        return self._objects.get(hash, None)
+
+    def set(self, hash, ob):
+        """Store the given object under the given hash.
+        Note that the cache does not have a (strong) ref to the object.
+        """
+        self._objects[hash] = ob
 
 
 class GfxSampler:
