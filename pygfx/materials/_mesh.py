d7f139c71f09147faf2a29eeaefa7b740cbf7f89
Pan Xinmiao <1092djhfg@163.com>
2023-08-14 09:10:54 +0800

    Update pygfx/materials/_mesh.py
    Co-authored-by: Korijn van Golen <korijn@gmail.com>

--- pygfx/materials/_mesh.py
+++ pygfx/materials/_mesh.py
@@ -338,7 +338,8 @@ class MeshBasicMaterial(MeshAbstractMaterial):
 
     @light_map.setter
     def light_map(self, map):
-        assert map is None or isinstance(map, Texture)
+        if map is not None and not isinstance(map, Texture):
+            raise ValueError(f"light_map must be a Texture or None, received: {type(map)}")
         self._store.light_map = map
 
     @property
