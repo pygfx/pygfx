e248a980efdc6e75176a4e4dd09ae2d060a7d023
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2024-03-01 13:16:27 +0100

    add pointer support, extend example with view control

--- examples/feature_demo/input_state.py
+++ examples/feature_demo/input_state.py
@@ -9,6 +9,7 @@ Example showing how to use the Input class.
 # sphinx_gallery_pygfx_target_name = "disp"
 
 import numpy as np
+import pylinalg as la
 import pygfx as gfx
 from wgpu.gui.auto import WgpuCanvas, run
 
@@ -59,22 +60,53 @@ camera.look_at((0, 2, 0))
 # add input
 input = gfx.Input(register_events=renderer)
 velocity = 0.1
+mouse_sensitivity = 0.01
+invert_x = -1
+invert_y = -1
+
+
+def update():
+    # TODO: technically we also need the time delta since the last frame
+    #       to be robust against varying frame rates
+    # TODO: clamp pitch to (-90, 90) degrees
+    # TODO: clamp pitch to (-90, 90) degrees
+    # TODO: how to always capture mouse events even if the pointer is not over the window?
+    # TODO: how to keep the mouse anchored to the center of the window?
+
+    # adjust camera angle based on mouse delta
+    rotation = input.pointer_delta()
+    if rotation != (0, 0):
+        pitch = la.quat_from_euler(
+            rotation[1] * mouse_sensitivity * invert_y, order="X"
+        )
+        yaw = la.quat_from_euler(rotation[0] * mouse_sensitivity * invert_x, order="Y")
+        camera.local.rotation = la.quat_mul(
+            la.quat_mul(
+                yaw,
+                camera.local.rotation,
+            ),
+            pitch,
+        )
+
+    # implement basic FPS controls
+    movement = np.array([0.0, 0.0, 0.0])
+    if input.key("w"):
+        movement += camera.local.forward * velocity
+    if input.key("a"):
+        movement += camera.local.right * -velocity
+    if input.key("d"):
+        movement += camera.local.right * velocity
+    if input.key("s"):
+        movement += camera.local.forward * -velocity
+
+    # TODO: project movement vector onto floor (fly vs walk; toggle?)
+
+    camera.local.position = camera.local.position + movement
 
 
 # setup animation loop
 def animate():
-    # implement basic FPS controls
-    movement = np.array([0.0, 0.0, 0.0])
-    if input.is_key_down("w"):
-        movement += camera.local.forward * velocity
-    if input.is_key_down("a"):
-        movement += camera.local.right * -velocity
-    if input.is_key_down("d"):
-        movement += camera.local.right * velocity
-    if input.is_key_down("s"):
-        movement += camera.local.forward * -velocity
-    camera.local.position = camera.local.position + movement
-
+    update()
     renderer.render(scene, camera)
     renderer.request_draw()
 
--- pygfx/controllers/_input.py
+++ pygfx/controllers/_input.py
@@ -3,8 +3,23 @@ from ..utils.viewport import Viewport
 
 class Input:
     def __init__(self, register_events=None):
-        self.key_down = {}
-        self.key_down = {}
+        # TODO:
+        # - controllers
+        # - virtual input state (e.g. buttons and axes, so users can remap controls)
+
+        self._key_down = {}
+        self._key_up = {}
+        self._key_state = {}
+
+        self._pointer_down = {}
+        self._pointer_up = {}
+        self._pointer_state = {}
+        self._pointer_pos = {}
+        self._pointer_pos_prev = {}
+        self._pointer_delta = {}
+
+        self._wheel_delta = {}
+
         if register_events is not None:
             self.register_events(register_events)
 
@@ -12,24 +27,94 @@ class Input:
         viewport = Viewport.from_viewport_or_renderer(viewport_or_renderer)
         viewport.renderer.add_event_handler(
             lambda event: self.handle_event(event, viewport),
-            # "pointer_down",
-            # "pointer_move",
-            # "pointer_up",
+            "pointer_down",
+            "pointer_move",
+            "pointer_up",
+            "wheel",
             "key_down",
             "key_up",
+            "after_flush",
         )
 
     def handle_event(self, event, viewport):
         type = event.type
 
-        if type.startswith("key_"):
-            # modifiers = {m.lower() for m in event.modifiers}
-            # modifiers.discard(event.key.lower())
+        if type == "after_flush":
+            self._pointer_pos_prev = self._pointer_pos.copy()
+            self._key_down.clear()
+            self._key_up.clear()
+            self._pointer_down.clear()
+            self._pointer_up.clear()
+            self._pointer_delta.clear()
+            self._wheel_delta.clear()
 
-            if event.type == "key_down":
-                self.key_down[event.key.lower()] = True
-            elif event.type == "key_up":
-                self.key_down[event.key.lower()] = False
+        elif type.startswith("pointer_"):
+            pointer_id = event.pointer_id
 
-    def is_key_down(self, key):
-        return self.key_down.get(key.lower(), False)
+            if type == "pointer_move":
+                x, y = event.x, event.y
+                self._pointer_pos[pointer_id] = (x, y)
+
+                if (prev := self._pointer_pos_prev.get(pointer_id)) is not None:
+                    prev_x, prev_y = prev
+                    self._pointer_delta[pointer_id] = (
+                        x - prev_x,
+                        y - prev_y,
+                    )
+            elif type == "pointer_down":
+                key = (pointer_id, event.button)
+                self._pointer_state[key] = True
+                self._pointer_down[key] = True
+            elif type == "pointer_up":
+                key = (pointer_id, event.button)
+                self._pointer_state[key] = False
+                self._pointer_up[key] = True
+
+        elif type.startswith("key_"):
+            key = event.key.lower()
+
+            if type == "key_down":
+                self._key_state[key] = True
+                self._key_down[key] = True
+            elif type == "key_up":
+                self._key_state[key] = False
+                self._key_up[key] = True
+
+        elif type == "wheel":
+            self._wheel_delta[event.pointer_id] = event.dy
+
+    def key_down(self, key):
+        """Returns True if the key_down event was received in the current frame"""
+        return self._key_down.get(key.lower(), False)
+
+    def key_up(self, key):
+        """Returns True if the key_up event was received in the current frame"""
+        return self._key_up.get(key.lower(), False)
+
+    def key(self, key):
+        """Returns True if the key is currently down"""
+        return self._key_state.get(key.lower(), False)
+
+    def pointer_button_down(self, button, pointer=0):
+        """Returns True if the pointer_down event was received in the current frame"""
+        return self._pointer_down.get((pointer, button), False)
+
+    def pointer_button_up(self, button, pointer=0):
+        """Returns True if the pointer_up event was received in the current frame"""
+        return self._pointer_up.get((pointer, button), False)
+
+    def pointer_button(self, button, pointer=0):
+        """Returns True if the pointer is currently down"""
+        return self._pointer_state.get((pointer, button), False)
+
+    def pointer(self, pointer=0):
+        """Returns the current position of a pointer"""
+        return self._pointer_pos.get(pointer, (0, 0))
+
+    def pointer_delta(self, pointer=0):
+        """Returns the movement of a pointer since the last frame"""
+        return self._pointer_delta.get(pointer, (0, 0))
+
+    def wheel(self, pointer=0):
+        """Returns the wheel delta of a pointer since the last frame"""
+        return self._wheel_delta.get(pointer, 0)
