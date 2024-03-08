d3b1c64aa72275d1f42ed09132d075b9d11516e2
Almar Klein <almar@almarklein.org>
2024-03-08 11:19:57 +0100

    bump version (#673)

--- pygfx/__init__.py
+++ pygfx/__init__.py
@@ -24,10 +24,10 @@ import pylinalg
 
 del pylinalg
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 version_info = tuple(map(int, __version__.split(".")))
 
-__wgpu_version_range__ = "0.14.1", "0.15.0"
+__wgpu_version_range__ = "0.14.1", "0.16.0"
 __pylinalg_version_range__ = "0.4.1", "0.5.0"
 
 
