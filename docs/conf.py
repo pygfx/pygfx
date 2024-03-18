e98dbb32ea64a789602c75d0d1aa0d159e772e0f
Almar Klein <almar@almarklein.org>
2024-03-18 12:39:36 +0100

    per-file gallery control

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,23 @@
 """Configuration script for Sphinx."""
 
 import os
+import re
 import sys
 import shutil
+import warnings
+from itertools import chain
+from pathlib import Path
+
 
 from sphinx_gallery.sorting import ExplicitOrder
 import wgpu.gui.offscreen
 
 
-ROOT_DIR = os.path.abspath(os.path.join(__file__, "..", ".."))
-sys.path.insert(0, ROOT_DIR)
+ROOT_DIR = Path(__file__).parents[1]  # repo root
+EXAMPLES_DIR = ROOT_DIR / "examples"
 
-# -- Sphix Gallery Hackz -----------------------------------------------------
-# When building the gallery, render offscreen and don't process
-# the event loop while parsing the example
+sys.path.insert(0, str(ROOT_DIR))
 
-
-def _ignore_offscreen_run():
-    wgpu.gui.offscreen.run = lambda: None
-
-
-os.environ["WGPU_FORCE_OFFSCREEN"] = "true"
-_ignore_offscreen_run()
 
 # -- Project information -----------------------------------------------------
 
@@ -31,6 +27,7 @@ author = "Almar Klein, Korijn van Golen"
 
 # The full version, including alpha/beta/rc tags
 # release = '0.1.0'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -56,10 +53,55 @@ shutil.rmtree(os.path.join(os.path.dirname(__file__), "_autosummary"), True)
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+
+# -- Sphix Gallery -----------------------------------------------------
+
+# Force offscreen rendering
+os.environ["WGPU_FORCE_OFFSCREEN"] = "true"
+
+# The Sphinx gallery does not have very good support to configure on a per-file
+# basis. So we do that ourselves. We collect lists of files, and then pour these
+# into regexp's for Sphinx to consume.
+examples_to_hide = []
+examples_to_show = []  # This is Sphinx' default, added for completenes, but not used.
+examples_to_run = []
+
+gallery_pattern = re.compile(r"^# example_gallery:(.+)$", re.MULTILINE)
+
+# Collect files
+for filename in EXAMPLES_DIR.glob("**/*.py"):
+    fname = str(filename.relative_to(EXAMPLES_DIR))
+    if "tests" in filename.parts:
+        continue
+    example_code = filename.read_text(encoding="UTF-8")
+    match = gallery_pattern.search(example_code)
+    config = match.group(1).lower().strip() if match else "<missing>"
+    if config == "hidden":
+        examples_to_hide.append(fname)
+    elif config == "code":
+        examples_to_show.append(fname)
+    elif config.startswith(("screenshot", "animate")):
+        examples_to_run.append(fname)
+    else:
+        examples_to_hide.append(fname)
+        warnings.warn(
+            f"Unexpected value for '# example_gallery: ' in {fname}: {config}."
+        )
+
+# Convert to regexp, because that's what Sphinx needs
+patternize = lambda path: (os.sep + path).replace("\\", "\\\\").replace(".", "\\.")
+sphinx_hide_pattern = "(" + "|".join(patternize(x) for x in examples_to_hide) + ")"
+sphinx_run_pattern = "(" + "|".join(patternize(x) for x in examples_to_run) + ")"
+
+# The gallery conf. See https://sphinx-gallery.github.io/stable/configuration.html
 sphinx_gallery_conf = {
     "examples_dirs": "../examples",
     "gallery_dirs": "_gallery",
     "backreferences_dir": "_gallery/backreferences",
+    "doc_module": ("pygfx",),
+    # Tell sphinx to use the scraper in the pygfx lib. This detects pygfx.utils.gallery_scraper.pygfx_scraper()
+    "image_scrapers": ("pygfx",),
+    # Don't show mini-galleries for these objects, because they include nearly all examples.
     "exclude_implicit_doc": {
         "WgpuRenderer",
         "Resource",
@@ -74,8 +116,7 @@ sphinx_gallery_conf = {
         "Scene",
         "Light",
     },
-    "doc_module": ("pygfx",),
-    "image_scrapers": ("pygfx",),
+    # Define order of appearance of the examples
     "subsection_order": ExplicitOrder(
         [
             "../examples/introductory",
@@ -84,9 +125,9 @@ sphinx_gallery_conf = {
             "../examples/other",
         ]
     ),
-    "remove_config_comments": True,
-    # Exclude files in 'other' dir from being executed
-    "filename_pattern": r"^((?![\\/]other[\\/]).)*$",
+    # Patterns to exclude and run examples, respectively
+    "ignore_pattern": sphinx_hide_pattern,
+    "filename_pattern": sphinx_run_pattern,
 }
 
 
