217bf8f6eae6cda4196d974cf07b942cb0255184
Almar Klein <almar@almarklein.org>
2024-03-28 12:44:52 +0100

    flake

--- examples/feature_demo/point_sprites.py
+++ examples/feature_demo/point_sprites.py
@@ -10,24 +10,23 @@ couple thousand of these in a plane, using a Points object.
 import numpy as np
 
 import pygfx as gfx
-from wgpu.gui.auto import WgpuCanvas, run
 
 
 # Create image of 'grass'
 im = np.zeros((20, 20, 4), np.uint8)
-im[:,:,1] = 255
+im[:, :, 1] = 255
 lengths = np.abs(np.random.normal(1, im.shape[1], (im.shape[0],)).astype(np.int32))
 for i, length in enumerate(lengths):
     im[:length, i, 3] = 100 if i % 2 else 20  # alternate high/low alpha
 
 # Create random positions
 positions = np.random.uniform(0, 100, size=(2000, 3)).astype(np.float32)
-positions[:,1] = 0
+positions[:, 1] = 0
 
 # Create point sprites
 grasses = gfx.Points(
     gfx.Geometry(positions=positions),
-    gfx.PointsSpriteMaterial(map=gfx.Texture(im, dim=2), size=3, size_space='world'),
+    gfx.PointsSpriteMaterial(map=gfx.Texture(im, dim=2), size=3, size_space="world"),
 )
 
 # Use a camera that is more inside the field, looking from above
