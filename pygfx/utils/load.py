b9dea8416e2fedd438d5e599be2460ff5338cdfa
Philipp Schlegel <observing@web.de>
2024-04-03 16:38:45 +0100

    improve load_meshes: - allow loading from remote sources (using trimesh.load_remote) - correctly convert scenes where geometries are re-used

--- pygfx/utils/load.py
+++ pygfx/utils/load.py
@@ -41,6 +41,7 @@ def load_meshes(path):
     ----------
     path : str
         The location where the mesh or scene is stored.
+        Can be a local file path or a URL.
 
     Returns
     -------
@@ -48,29 +49,52 @@ def load_meshes(path):
         A list of loaded meshes.
 
     """
-
     import trimesh  # noqa
 
-    scene = trimesh.load(path)
+    # Trimesh's load() performs a similar check and refers
+    # loading from URLs to load_remote()
+    if "https://" in str(path) or "http://" in str(path):
+        scene = trimesh.load_remote(path)
+    else:
+        scene = trimesh.load(path)
+
+    # If this is a single object we can just convert and return it
     if isinstance(scene, trimesh.Trimesh):
         m = gfx.Mesh(
             gfx.geometry_from_trimesh(scene),
             gfx.MeshPhongMaterial(),
         )
         return [m]
-
+    # If this is a scene, we need to properly parse it
     elif isinstance(scene, trimesh.Scene):
+        # Scene consists of geometries (which in trimesh contain both
+        # the vertices/faces as well as the mateiral) and a scene graph.
+        # Each node in the graph consists of a geometry and a transform
+        # A geometry can be used by more than one node in the graph.
+        # Here we will convert each node in the graph to a pygfx.Mesh
+        # but in the future it might be better to use pygfx.InstancedMesh.
+        gfx_geometries = {}
+        gfx_materials = {}
+        meshes = []
         for node_name in scene.graph.nodes_geometry:
             transform, geometry_name = scene.graph[node_name]
-            current = scene.geometry[geometry_name]
-            current.apply_transform(transform)
 
-        return [
-            gfx.Mesh(
-                gfx.geometry_from_trimesh(m),
-                gfx.material_from_trimesh(m.visual.material),
-            )
-            for m in scene.geometry.values()
-        ]
+            # Convert each geometry only once
+            if geometry_name not in gfx_geometries:
+                m = scene.geometry[geometry_name]
+                gfx_geometries[geometry_name] = gfx.geometry_from_trimesh(m)
+                if hasattr(m.visual, "material"):
+                    gfx_materials[geometry_name] = gfx.material_from_trimesh(
+                        m.visual.material
+                    )
+                else:
+                    gfx_materials[geometry_name] = gfx.MeshStandardMaterial()
+
+            # Generate a visual for each node
+            mesh = gfx.Mesh(gfx_geometries[geometry_name], gfx_materials[geometry_name])
+            mesh.local.matrix = transform  # set the node's transform
+
+            meshes.append(mesh)
+        return meshes
     else:
         raise ValueError(f"Unexpected trimesh data: {scene.__class__.__name__}")
