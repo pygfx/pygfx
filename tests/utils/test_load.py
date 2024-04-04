aa8577aea841ce94c1f294fa45c3dc4d1a811ef4
Philipp Schlegel <observing@web.de>
2024-04-04 18:12:39 +0100

    fix load tests

--- tests/utils/test_load.py
+++ tests/utils/test_load.py
@@ -5,10 +5,10 @@ import pygfx as gfx
 
 BASE_URL = "https://github.com/mikedh/trimesh/raw/main/models"
 FILES_TO_TEST = [
-            "wallhole.obj",
-            "CesiumMilkTruck.glb",
-            "nested.glb",
-            "cube_blender_uv.ply"
+    "wallhole.obj",
+    "CesiumMilkTruck.glb",
+    "nested.glb",
+    "cube_blender_uv.ply",
 ]
 URLS = [f"{BASE_URL}/{file}" for file in FILES_TO_TEST]
 
@@ -16,15 +16,16 @@ URLS = [f"{BASE_URL}/{file}" for file in FILES_TO_TEST]
 @pytest.mark.parametrize("url", URLS)
 def test_load_meshes(url):
     # Test loading meshes via trimesh
-    mesh = gfx.load_meshes(url, remote_ok=True)
+    mesh = gfx.load_mesh(url, remote_ok=True)
 
     assert isinstance(mesh, list)
     assert all([isinstance(m, gfx.Mesh) for m in mesh])
 
 
 @pytest.mark.parametrize("url", URLS)
-def test_load_scenes(url):
+@pytest.mark.parametrize("flatten", (True, False))
+def test_load_scenes(url, flatten, ):
     # Test loading scenes via trimesh
-    mesh = gfx.load_scene(url, remote_ok=True)
+    mesh = gfx.load_scene(url, flatten=flatten, remote_ok=True)
 
     assert isinstance(mesh, gfx.Scene)
