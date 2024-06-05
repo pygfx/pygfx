dfa74246e5c9e7ec813c33649ba2af4e9be62e79
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-05 10:43:15 -0400

    Move text slightly in front of the outline. (#776)
    * Move text outline backward
    
    * Add a validation example
    
    * new screenshot
    
    * Reduce outline thickness
    
    * Update screenshot
    
    ---------
    
    Co-authored-by: Almar Klein <almar@almarklein.org>

---
+++ examples/validation/validate_text_outline.py
@@ -0,0 +1,46 @@
+"""
+Text outline always behind text itself
+======================================
+
+Example demonstrating the capabilities of text outline to stay behind the text
+itself, even for very thick value of the outline thickness where the outline of
+one character may overlap with the neighboring one.
+"""
+
+# sphinx_gallery_pygfx_docs = 'screenshot'
+# sphinx_gallery_pygfx_test = 'compare'
+
+
+from wgpu.gui.auto import WgpuCanvas, run
+import pygfx as gfx
+
+scene = gfx.Scene()
+
+
+scene.add(gfx.Background.from_color("#fff", "#000"))
+
+text = gfx.Text(
+    gfx.TextGeometry(
+        text="Hello world",
+        font_size=100,
+        screen_space=True,
+        text_align="right",
+        anchor="center",
+    ),
+    gfx.TextMaterial(
+        color="#DA9DFF",
+        outline_color="#000",
+        # Choose a very thick outline to ensure the effect is noticeable
+        outline_thickness=0.45,
+    ),
+)
+
+scene.add(text)
+
+camera = gfx.OrthographicCamera(4, 3)
+renderer = gfx.renderers.WgpuRenderer(WgpuCanvas(size=(800, 600)))
+
+renderer.request_draw(lambda: renderer.render(scene, camera))
+
+if __name__ == "__main__":
+    run()
