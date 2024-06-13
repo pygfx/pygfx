df4399e9a88319bb95b270bf6812463c03dd9080
Almar Klein <almar@almarklein.org>
2024-06-13 10:28:53 +0200

    allow for new and old wgpu-py

--- pygfx/renderers/wgpu/engine/shared.py
+++ pygfx/renderers/wgpu/engine/shared.py
@@ -267,7 +267,10 @@ class PyGfxAdapterInfoDiagnostics(wgpu.DiagnosticsBase):
     def get_dict(self):
         shared = get_shared()
         adapter = shared.adapter
-        return adapter.info.copy()
+        if hasattr(adapter, "request_adapter_info"):  # wgpu-py < 0.16
+            return adapter.request_adapter_info()
+        else:
+            return adapter.info
 
 
 class PyGfxFeaturesDiagnostics(wgpu.DiagnosticsBase):
