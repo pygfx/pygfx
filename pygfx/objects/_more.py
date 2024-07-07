23a2b53e1e230276c1b471f813449190ac76f1b8
Mark Harfouche <mark.harfouche@gmail.com>
2024-07-07 10:03:47 -0400

    Add the name property to more objects
    I found this while trying to troubleshoot some code of mine.

--- pygfx/objects/_more.py
+++ pygfx/objects/_more.py
@@ -17,13 +17,15 @@ class Group(WorldObject):
     ----------
     visible : bool
         If true, the object and its children are visible.
-    position : Vector
-        The position of the object in the world. Default (0, 0, 0).
+
+    name : str
+        The name of the group.
 
     """
 
-    def __init__(self, *, visible=True):
+    def __init__(self, *, visible=True, name=""):
         super().__init__(visible=visible)
+        self.name = name
 
 
 class Scene(Group):
@@ -35,8 +37,8 @@ class Scene(Group):
 
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class Background(WorldObject):
@@ -335,6 +337,8 @@ class Text(WorldObject):
         recommended to let the renderer decide, using "auto".
     position : Vector
         The position of the object in the world. Default (0, 0, 0).
+    name : str
+        The name of the text object for inspection and debugging.
 
     """
 
@@ -351,6 +355,7 @@ class Text(WorldObject):
         visible=True,
         render_order=0,
         render_mask="auto",
+        name="",
     ):
         super().__init__(
             geometry,
@@ -358,6 +363,7 @@ class Text(WorldObject):
             visible=visible,
             render_order=render_order,
             render_mask=render_mask,
+            name=name,
         )
 
         # calling super from callback is possible, but slow so we register it as a second callback instead
