509219c2005e48452471d5f7fad348be73271f13
kushalkolar <kushalkolar@gmail.com>
2024-07-10 23:14:38 -0400

    unproject to get range of screen space to set rulers

--- examples/feature_demo/line_plot.py
+++ examples/feature_demo/line_plot.py
@@ -11,6 +11,7 @@ Show a line plot, with axii and a grid.
 import numpy as np
 from wgpu.gui.auto import WgpuCanvas, run
 import pygfx as gfx
+from pylinalg import vec_transform, vec_unproject
 
 
 canvas = WgpuCanvas()
@@ -53,14 +54,38 @@ camera.show_rect(0, 1000, -5, 5)
 controller = gfx.PanZoomController(camera, register_events=renderer)
 
 
+def map_screen_to_world(pos, viewport_size):
+    # first convert position to NDC
+    x = pos[0] / viewport_size[0] * 2 - 1
+    y = -(pos[1] / viewport_size[1] * 2 - 1)
+    pos_ndc = (x, y, 0)
+
+    pos_ndc += vec_transform(camera.world.position, camera.camera_matrix)
+    # unproject to world space
+    pos_world = vec_unproject(pos_ndc[:2], camera.camera_matrix)
+
+    return pos_world
+
+
 def animate():
-    rulerx.start_pos = camera.world.x - camera.width / 2, 0, -1000
-    rulerx.end_pos = camera.world.x + camera.width / 2, 0, -1000
+    # get range of screen space
+    xmin, ymin = 0, renderer.logical_size[1]
+    xmax, ymax = renderer.logical_size[0], 0
+
+    world_xmin, world_ymin, _ = map_screen_to_world((xmin, ymin), renderer.logical_size)
+    world_xmax, world_ymax, _ = map_screen_to_world((xmax, ymax), renderer.logical_size)
+
+    # set start and end positions of rulers
+    rulerx.start_pos = world_xmin, 0, -1000
+    rulerx.end_pos = world_xmax, 0, -1000
+
     rulerx.start_value = rulerx.start_pos[0]
+
     statsx = rulerx.update(camera, canvas.get_logical_size())
 
-    rulery.start_pos = 0, camera.world.y - camera.height / 2, -1000
-    rulery.end_pos = 0, camera.world.y + camera.height / 2, -1000
+    rulery.start_pos = 0, world_ymin, -1000
+    rulery.end_pos = 0, world_ymax, -1000
+
     rulery.start_value = rulery.start_pos[1]
     statsy = rulery.update(camera, canvas.get_logical_size())
 
@@ -71,6 +96,9 @@ def animate():
     # print(statsx)
 
     renderer.render(scene, camera)
+
+
+camera.zoom = 0.5
 
 
 if __name__ == "__main__":
