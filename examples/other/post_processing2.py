9162573ca54607805cdb5949da5a4be0463b9652
Almar Klein <almar@almarklein.org>
2024-09-26 11:42:03 +0200

    Fix cases where ruff and black disagree

--- examples/other/post_processing2.py
+++ examples/other/post_processing2.py
@@ -51,10 +51,11 @@ class MyRenderFlusher(gfx.renderers.wgpu._renderutils.RenderFlusher):
 
     def __init__(self, device):
         super().__init__(device)
-        self._shader["tex_coord_map"] = """
+        code = """
             let a = u_render.amplitude;
             tex_coord.x = tex_coord.x + sin(tex_coord.y * 20.0) * a;
-            """
+        """
+        self._shader["tex_coord_map"] = code
         self._uniform_data["amplitude"] = 0.02
 
 
