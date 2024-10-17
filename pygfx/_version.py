c5982acc13778f9fa5182547dbcf1e7c3609828c
Almar Klein <almar@almarklein.org>
2024-10-17 10:30:21 +0200

    Align with wgpu

--- pygfx/_version.py
+++ pygfx/_version.py
@@ -1,6 +1,6 @@
 """
-Versioning for Pygfx. We use a hard-coded version number, because it's simple
-and alwayd works. And for dev installs we add extra versioning info.
+Versioning: we use a hard-coded version number, because it's simple and always
+works. For dev installs we add extra version info from Git.
 """
 
 import logging
@@ -9,11 +9,14 @@ from pathlib import Path
 
 
 # This is the reference version number, to be bumped before each release.
-# The build system (Flit) detects this definition when building a distribution.
+# The build system detects this definition when building a distribution.
 __version__ = "0.5.0"
 
+# Allow using nearly the same code in different projects
+project_name = "pygfx"
 
-logger = logging.getLogger("pygfx")
+
+logger = logging.getLogger(project_name.lower())
 
 # Get whether this is a repo. If so, repo_dir is the path, otherwise repo_dir is None.
 repo_dir = Path(__file__).parents[1]
@@ -40,7 +43,9 @@ def get_extended_version():
     if not release:
         release = base_release
     elif release != base_release:
-        logger.warning("Pygfx version from git and __version__ don't match.")
+        logger.warning(
+            f"{project_name} version from git ({release}) and __version__ ({base_release}) don't match."
+        )
 
     # Build the total version
     version = release
@@ -73,7 +78,7 @@ def get_version_info_from_git():
     try:
         p = subprocess.run(command, cwd=repo_dir, capture_output=True)
     except Exception as e:
-        logger.warning("Could not get pygfx version: " + str(e))
+        logger.warning(f"Could not get {project_name} version: {e}")
         p = None
 
     # Parse the result into parts
@@ -84,7 +89,7 @@ def get_version_info_from_git():
         if p.returncode:
             stderr = p.stderr.decode(errors="ignore")
             logger.warning(
-                "Could not get pygfx version.\n\nstdout: "
+                f"Could not get {project_name} version.\n\nstdout: "
                 + output
                 + "\n\nstderr: "
                 + stderr
@@ -102,3 +107,7 @@ def get_version_info_from_git():
 
 
 __version__ = get_version()
+
+version_info = tuple(
+    int(i) if i.isnumeric() else i for i in __version__.split("+")[0].split(".")
+)
