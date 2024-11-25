6dc9ed91154a46eb9b46befc4c76df9be9602580
Almar Klein <almar@almarklein.org>
2024-11-25 17:07:26 +0100

    Change the example

--- examples/feature_demo/image_plus_points_yuv.py
+++ examples/feature_demo/image_plus_points_yuv.py
@@ -1,122 +1,121 @@
-"""
-Image YUV420 with Points Overlaid
-=================================
-
-Show an image tranmitted to the GPU in YUV colorspace with points overlaid.
-This show an example of how to write a shader that is able to combine data
-from multiple grids into one.
-"""
+""" """
 
 # sphinx_gallery_pygfx_docs = 'screenshot'
 # sphinx_gallery_pygfx_test = 'run'
 
-import imageio.v3 as iio
-from wgpu.gui.auto import WgpuCanvas, run
-import pygfx as gfx
+import time
+
+import wgpu
 import numpy as np
+from wgpu.gui.auto import WgpuCanvas, run
+from wgpu.gui.offscreen import WgpuCanvas as OffscreenCanvas
+import pygfx as gfx
+import imageio
+import av
 
 
-canvas = WgpuCanvas()
-renderer = gfx.renderers.WgpuRenderer(canvas)
-scene = gfx.Scene()
-
-# add image
-
-image_rgb = iio.imread("imageio:astronaut.png")
+OFFSCREEN = False  # Set to True if FPS is capped at 60 or 120
+FILENAME = imageio.core.Request("imageio:cockatoo.mp4", "r?").filename
+FORMAT = "yuv420p"  # Set to yuv420p or rgb24
 
 
-def rgb_to_yuv420_limited_range(im):
-    """A pure python implementation of cv2.cvtColor(im, cv2.COLOR_RGB2YUV_I420)"""
-    # import cv2
-    # image_yuv420 = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2YUV_I420)
-    # Define conversion matrix for RGB to YUV limited range (ITU-R BT.601)
-    m = np.array(
-        [[0.257, 0.504, 0.098], [-0.148, -0.291, 0.439], [0.439, -0.368, -0.071]]
+def video_width_height():
+    container = av.open(FILENAME)
+    return (container.streams[0].width, container.streams[0].height)
+
+
+def loop_video():
+    while True:
+        container = av.open(FILENAME)
+        for frame in container.decode(video=0):
+            # Reformat if necessary. If format is the same, reformat is a no-op.
+            # Otherwise, this includes a data copy and some computations.
+            # In the case of yuv420 -> rgb24, the image also takes more memory.
+            frame.reformat(format=FORMAT)
+            # Cast to numpy array. This should not involve a data copy.
+            yield frame.to_ndarray()
+
+
+w, h = video_width_height()
+frame_generator = loop_video()
+
+# Create image object
+
+if FORMAT == "yuv420p":
+    # For planar yuv420, we use a 2-layer luminance texture. The u and v planes
+    # fit in a single layer since they're half the size (in both dimensions).
+    tex = gfx.Texture(
+        size=(w, h, 2),
+        dim=2,
+        colorspace="yuv420p",
+        format="r8unorm",
+        usage=wgpu.TextureUsage.COPY_DST,
     )
-    offset = np.array([16, 128, 128])
-    im = im.astype(np.float32)
-    yuv = im @ m.T + offset
-    yuv = np.clip(yuv, [16, 16, 16], [235, 240, 240])
+else:
+    # For rgb we use an rgba texture (there is no rgb texture format).
+    tex = gfx.Texture(
+        size=(w, h, 1),
+        dim=2,
+        colorspace="srgb",
+        format="rgba8unorm",
+        usage=wgpu.TextureUsage.COPY_DST,
+    )
 
-    yuv = yuv.astype(np.uint8)
-    y = yuv[..., 0]
-    u = yuv[::2, ::2, 1].reshape(-1, y.shape[1])
-    v = yuv[::2, ::2, 2].reshape(-1, y.shape[1])
-    return np.vstack([y, u, v])
-
-
-# OpenCV returns a "planar format" YUV where
-# the first "plane" of data "Y" is the intensity
-# YUV420 subsamples the intensity and has the same "shape"
-# as original image
-# The U and V planes follow but are "subsampled" by 2
-# in each direction.
-# So they are 1/4 of the size
-# Extract them, then reshape them.
-height, width = image_rgb.shape[0:2]
-u_height = height // 4
-
-image_yuv420 = rgb_to_yuv420_limited_range(image_rgb)
-
-im_y = image_yuv420[:height]
-im_u = image_yuv420[height : height + u_height]
-im_v = image_yuv420[height + u_height :]
-# Rehsape can cause "implicit" copies of the data, so we assign the shape which causes the data to never get copied
-im_u.shape = height // 2, width // 2
-im_v.shape = height // 2, width // 2
-
-
-uv_shape = height // 2, width // 2
-image_2layer = np.zeros((2, height, width), np.uint8)
-image_2layer[0,] = image_yuv420[:height]
-image_2layer[1, : height // 2, : width // 2] = image_yuv420[
-    height : height + u_height
-].reshape(uv_shape)
-image_2layer[1, height // 2 :, : width // 2] = image_yuv420[
-    height + u_height :
-].reshape(uv_shape)
-
-
-image = gfx.Image(
-    gfx.Geometry(
-        grid=gfx.Texture(
-            image_2layer, dim=2, size=(width, height, 2), colorspace="yuv420"
-        ),
-        # grid=gfx.Texture(image_2layer, dim=2),
-        # grid_u=gfx.Texture(im_u, dim=2),
-        # grid_v=gfx.Texture(im_v, dim=2),
-    ),
-    gfx.ImageBasicMaterial(
-        clim=(0, 255),
-    ),
+im = gfx.Image(
+    gfx.Geometry(grid=tex),
+    gfx.ImageBasicMaterial(clim=(0, 255)),
 )
-scene.add(image)
 
-# add points
-xx = [182, 180, 161, 153, 191, 237, 293, 300, 272, 267, 254]
-yy = [145, 131, 112, 59, 29, 14, 48, 91, 136, 137, 172]
-sizes = np.arange(1, len(xx) + 1, dtype=np.float32)
+# Setup the rest of the viz
 
-points = gfx.Points(
-    gfx.Geometry(
-        positions=[(x, y, 1) for x, y in zip(xx, yy)],
-        sizes=sizes,
-    ),
-    gfx.PointsMaterial(
-        color=(0, 1, 1, 1),
-        size=10,
-        size_space="world",
-        size_mode="vertex",
-    ),
-)
-scene.add(points)
-
-camera = gfx.PerspectiveCamera(0)
+CanvasClass = OffscreenCanvas if OFFSCREEN else WgpuCanvas
+canvas = CanvasClass(max_fps=999, vsync=False)
+renderer = gfx.renderers.WgpuRenderer(canvas, show_fps=True)
+scene = gfx.Scene()
+scene.add(im)
+camera = gfx.OrthographicCamera()
+camera.show_object(scene)
 camera.local.scale_y = -1
-camera.show_rect(-10, 522, -10, 522)
+stats = gfx.Stats(viewport=renderer)
 
-controller = gfx.PanZoomController(camera, register_events=renderer)
+
+def animate():
+    # Get next frame to upload
+    data = next(frame_generator)
+
+    if FORMAT == "yuv420p":
+        # Send the three planes to the texture. The y-plane goes to the first layer.
+        # The u-plane and v-plane go to the second layer, side-by side.
+        # Note that the u and v planes are just a quarter of the size of the y-plane.
+        # All planes are contiguous, so there are zero data copies.
+        y = data[:h]
+        u = data[h : h + h // 4].reshape(h // 2, w // 2)
+        v = data[h + h // 4 :].reshape(h // 2, w // 2)
+        tex.send_data((0, 0, 0), y)
+        tex.send_data((0, 0, 1), u)
+        tex.send_data((w // 2, 0, 1), v)
+    else:
+        # We need to copy the rgb data to rgba, beause wgpu does not have rgb
+        # textures. Note that you can set textures with rgb data, and then the
+        # Texture makes a copy automatically upon upload, similar to the below.
+        rgba = np.full((*data.shape[:2], 4), 255, dtype=np.uint8)
+        rgba[:, :, :3] = data
+        tex.send_data((0, 0, 0), rgba)
+
+    with stats:
+        renderer.render(scene, camera, flush=False)
+    stats.render()
+
+    canvas.request_draw()
+
 
 if __name__ == "__main__":
-    canvas.request_draw(lambda: renderer.render(scene, camera))
-    run()
+    renderer.request_draw(animate)
+    if OFFSCREEN:
+        # Just render as fast as we can, for 5 secs, to measure fps
+        etime = time.time() + 5
+        while time.time() < etime:
+            canvas.draw()
+    else:
+        # Enter normal canvas event loop
+        run()
