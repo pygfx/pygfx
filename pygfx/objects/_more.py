99ab0faed0dba65d8495c99561201a8255936706
Korijn van Golen <korijn@gmail.com>
2024-12-22 17:07:33 +0100

    ensure text also doesnt trigger eager re-eval of world matrix

--- pygfx/objects/_more.py
+++ pygfx/objects/_more.py
@@ -434,12 +434,8 @@ class Text(WorldObject):
             name=name,
         )
 
-        # calling super from callback is possible, but slow so we register it as a second callback instead
-        self.world.on_update(super()._update_uniform_buffers)
-
-    @callback
     def _update_uniform_buffers(self, transform: AffineBase):
-        # super()._update_uniform_buffers(transform)
+        super()._update_uniform_buffers()
         # When rendering in screen space, the world transform is used
         # to establish the point in the scene where the text is placed.
         # The only part of the local transform that is used is the
