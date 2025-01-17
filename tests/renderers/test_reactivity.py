5d1852bc1ae8aedff9dfb5cd6d3e8c2f5ce74c7d
Almar Klein <almar@almarklein.org>
2025-01-17 11:55:38 +0100

    Associate pipeline-container also with Material (#939)
    * Associate pipeline-container also with Material
    
    * fix and tweaks
    
    * Fix tests
    
    * Add comment
    
    * Refactor trackable.py a bit: root -> tracker
    
    * Add docs
    
    * Add tests
    
    ---------
    
    Co-authored-by: Korijn van Golen <korijn@gmail.com>

--- tests/renderers/test_reactivity.py
+++ tests/renderers/test_reactivity.py
@@ -75,7 +75,7 @@ def test_reactivity_mesh1():
     # Changing the side requires a new pipeline
     cube.material.side = "front"
     changed = render(cube)
-    assert changed == {"pipeline_info", "render_info", "compose_pipeline"}
+    assert changed == {"pipeline_info", "render_info", "pipeline"}
 
     # Changing the wireframe requires a new shader
     cube.material.wireframe = True
@@ -93,7 +93,7 @@ def test_reactivity_mesh2():
         gfx.MeshPhongMaterial(color="#336699"),
     )
 
-    m1 = cube.material  # noqa
+    m1 = cube.material
     m2 = gfx.MeshPhongMaterial(color="#ff6699")
     m3 = gfx.MeshBasicMaterial(color="#11ff99")
 
@@ -109,17 +109,32 @@ def test_reactivity_mesh2():
     # Render once
     render(cube)
 
-    # Swap out the material - same type
+    # Swap out the material - same type, still new container
     cube.material = m2
     changed = render(cube)
-    assert changed == {"bindings"}
+    assert "create" in changed
+    assert "shader" in changed
+    assert "pipeline" in changed
 
     # Swap out the material - different type
     cube.material = m3
     changed = render(cube)
     assert "create" in changed
-    assert "compile_shader" in changed
-    assert "compose_pipeline" in changed
+    assert "shader" in changed
+    assert "pipeline" in changed
+
+    # But swapping to a previously used material is free
+    cube.material = m1
+    changed = render(cube)
+    assert not changed
+
+    cube.material = m2
+    changed = render(cube)
+    assert not changed
+
+    cube.material = m3
+    changed = render(cube)
+    assert not changed
 
     # Swap out the positions
     cube.geometry.positions = p2
@@ -159,7 +174,72 @@ def test_reactivity_mesh3():
     print("uv", geometry.texcoords.data.shape)
     print("map", obj.material.map.texture.dim)
     changed = render(obj)
-    assert changed == {"bindings", "compile_shader", "compose_pipeline"}
+    assert changed == {"bindings", "shader", "pipeline"}
+
+
+def test_reactivity_mesh4():
+    # Test basics with shared materials
+
+    # Prepare
+
+    m1 = gfx.MeshPhongMaterial(color="#00ff99")
+    m2 = gfx.MeshPhongMaterial(color="#ff6699")
+
+    cube1 = gfx.Mesh(gfx.box_geometry(200, 200, 200), m1)
+    cube2 = gfx.Mesh(gfx.box_geometry(200, 200, 200), m1)
+
+    # Render once
+    render(cube1)
+    render(cube2)
+
+    # Changing the color should not change anything
+    cube1.material.color = "red"
+    changed = render(cube1)
+    assert changed == set()
+    changed = render(cube2)
+    assert changed == set()
+
+    # Changing the wireframe requires a new shader
+    cube1.material.wireframe = True
+    changed = render(cube1)
+    assert "shader" in changed
+    changed = render(cube2)
+    assert "shader" in changed
+
+    # Now change material for one object
+    cube1.material = m2
+
+    changed = render(cube1)
+    assert "create" in changed
+    changed = render(cube2)
+    assert changed == set()
+
+    # Swap the other too, to prime it
+    cube2.material = m2
+
+    changed = render(cube1)
+    assert changed == set()
+    changed = render(cube2)
+    assert "create" in changed
+
+    # Now assign different materials
+    cube1.material = m1
+    cube2.material = m2
+
+    assert not render(cube1)
+    assert not render(cube2)
+
+    # And change the wireframe back of m1
+    cube1.material.wireframe = False
+
+    assert "shader" in render(cube1)
+    assert not render(cube2)
+
+    # And when we change cube2 to use that material ... it detects the change
+    cube2.material = m1
+
+    assert not render(cube1)
+    assert "shader" in render(cube2)
 
 
 def test_change_blend_mode():
