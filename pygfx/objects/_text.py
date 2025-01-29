a4b92e6b214c2f92b327978fd8b1d52dd5377b24
Almar Klein <almar@almarklein.org>
2025-01-29 15:54:31 +0100

    props

--- pygfx/objects/_text.py
+++ pygfx/objects/_text.py
@@ -68,20 +68,22 @@ class Text(WorldObject):
 
     @property
     def family(self):
-        return self.geometry._family
+        """The font family to use.
+
+        The name(s) of the font to prefer. If multiple names are given, they are
+        preferred in the given order. Characters that are not supported by any
+        of the given fonts are rendered with the default font (from the Noto
+        Sans collection).
+        """
+        return self.geometry._font_props.family
 
     @family.setter
     def family(self, family):
         if family is None:
-            self.geometry._family = None
             self.geometry._font_props = textmodule.FontProps()
         else:
             self.geometry._font_props = textmodule.FontProps(family)
-            self._family = family
-        # Trigger update in items that use the geometry's value
-        for item in self._text_items:
-            if item.family is None:
-                item.family = None
+        self._trigger_items_update()
 
     @property
     def font_size(self):
@@ -108,14 +110,149 @@ class Text(WorldObject):
     @font_size.setter
     def font_size(self, value):
         self.geometry._font_size = float(value)
-        # Trigger update in items that use the geometry's value
-        for item in self._text_items:
-            if item.font_size is None:
-                item.font_size = None
+        self._trigger_items_update()
 
-        # self.apply_layout()
+    @property
+    def max_width(self):
+        """The maximum width of the text. Text will wrap if beyond this
+        limit. The coordinate system that this applies to is the same
+        as for ``font_size``. Set to 0 for no wrap. Default 0.
+        """
+        return self.geometry._max_width
+
+    @max_width.setter
+    def max_width(self, max_width):
+        self.geometry._max_width = float(max_width)
+        self._trigger_items_update()
+
+    @property
+    def line_height(self):
+        """The relative height of a line of text, used to set the
+        distance between lines. Default 1.2.
+        """
+        return self.geometry._line_height
+
+    @line_height.setter
+    def line_height(self, height):
+        self.geometry._line_height = float(height or 1.2)
+        self._trigger_items_update()
+
+    @property
+    def text_align(self):
+        """Set the alignment of wrapped text. Can be start, end, or center.
+        Default "start".
+
+        Text alignment is ignored for top to bottom ('ttb') and
+        bottom to top ('btt') directions.
+        """
+        return self.geometry._text_align
+
+    @text_align.setter
+    def text_align(self, align):
+        if align is None:
+            align = "start"
+        if not isinstance(align, str):
+            raise TypeError("text-align must be a None or str.")
+        align = align.lower()
+        if align not in _TEXT_ALIGNMENTS:
+            raise ValueError(f"Align must be one of {_TEXT_ALIGNMENTS}. Got {align}.")
+        self.geometry._text_align = align
+        self._trigger_items_update()
+
+    @property
+    def text_align_last(self):
+        """Set the alignment of the last line of text.
+        Default "auto".
+
+        Text alignment is ignored for top to bottom ('ttb') and
+        bottom to top ('btt') directions.
+        """
+        return self.geometry._text_align_last
+
+    @text_align_last.setter
+    def text_align_last(self, align):
+        if align is None:
+            align = "start"
+        if not isinstance(align, str):
+            raise TypeError("text-align must be a None or str.")
+        align = align.lower()
+        if align not in _TEXT_ALIGNMENTS_LAST:
+            raise ValueError(
+                f"Align must be one of {_TEXT_ALIGNMENTS_LAST}. Got {align}"
+            )
+        self.geometry._text_align_last = align
+        self._trigger_items_update()
+
+    @property
+    def anchor(self):
+        """The position of the origin of the text. This is a string
+        representing the vertical and horizontal anchors, separated by
+        a dash, e.g. "top-left" or "bottom-center".
+
+        * Vertical values: "top", "middle", "baseline", "bottom".
+        * Horizontal values: "left", "center", "right".
+        """
+        return self.geometry._anchor
+
+    @anchor.setter
+    def anchor(self, anchor):
+        # Init
+        if anchor is None:
+            anchor = "middle-center"
+        elif not isinstance(anchor, str):
+            raise TypeError("Text anchor must be str.")
+        anchor = anchor.lower().strip()
+        # Split
+        if anchor.count("-") == 1:
+            anchory, _, anchorx = anchor.partition("-")
+        else:
+            anchory = anchorx = ""
+            for key, val in ANCHOR_Y_ALTS.items():
+                if anchor.startswith(key):
+                    anchory = val
+                    break
+            for key, val in ANCHOR_X_ALTS.items():
+                if anchor.endswith(key):
+                    anchorx = val
+                    break
+        # Resolve
+        try:
+            anchory = ANCHOR_Y_ALTS[anchory]
+            anchorx = ANCHOR_X_ALTS[anchorx]
+        except KeyError:
+            raise ValueError(f"Invalid anchor value '{anchor}'") from None
+        # Apply
+        self.geometry._anchor = f"{anchory}-{anchorx}"
+        self._trigger_items_update()
+
+    @property
+    def anchor_offset(self):
+        """The offset (extra margin) for the 'top', 'bottom', 'left', and 'right' anchors."""
+        return self.geometry._anchor_offset
+
+    @anchor_offset.setter
+    def anchor_offset(self, value):
+        self.geometry._anchor_offset = float(value)
+        self._trigger_items_update()
+
+    def set_text(self, text):
+        """Set the text for this TextItem."""
+        # TODO: could be a settable property?
+        # Convert text to words
+
+        if text == self._text or text is None:
+            return
+        if not isinstance(text, str):
+            raise TypeError("TextItem text should be str.")
+        self._text = text
+
+        self._text2words()
 
     # ---
+
+    def _trigger_items_update(self):
+        for item in self._text_items:
+            item._mark_dirty()
 
     def _ensure_text_item_count(self, n):
         """Allocate new buffer if necessary."""
@@ -157,7 +294,7 @@ class Text(WorldObject):
         # Update items
         for index in dirty_items:
             item = self._text_items[index]
-            self.geometry._update_item(item)
+            item._update()
         # Reset
         dirty_items.clear()
 
