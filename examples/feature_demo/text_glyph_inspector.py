c04cafd41a5bf975f67bd0b322eaabf29af0005d
Mark Harfouche <mark.harfouche@gmail.com>
2025-03-04 08:36:47 -0500

    Fixup the glyph inspector

--- examples/feature_demo/text_glyph_inspector.py
+++ examples/feature_demo/text_glyph_inspector.py
@@ -10,18 +10,12 @@ on the text glyphs.
 # sphinx_gallery_pygfx_test = 'run'
 
 import argparse
-import sys
-from pathlib import Path
 
-import imageio.v3 as imageio
-import numpy as np
 from imgui_bundle import imgui
 from wgpu.gui.auto import WgpuCanvas, run
 from wgpu.utils.imgui import ImguiRenderer
 
 import pygfx as gfx
-from pygfx.renderers.wgpu import register_wgpu_render_function
-from pygfx.renderers.wgpu.shaders.imageshader import ImageShader
 
 # Add argparse to take in the input text to inspect
 
@@ -31,7 +25,7 @@ args = parser.parse_args()
 
 text_to_display = args.text
 
-canvas_size = 800, 600
+canvas_size = 800, 800
 
 canvas = WgpuCanvas(size=canvas_size)
 renderer = gfx.renderers.WgpuRenderer(canvas)
@@ -41,21 +35,7 @@ w, h = canvas.get_logical_size()
 viewport_atlas = gfx.Viewport(renderer, rect=(0, 0, w // 2, h))
 viewport_text = gfx.Viewport(renderer, rect=(w // 2, 0, w - w // 2, h))
 
-atlas_scene = gfx.Scene()
-atlas_scene.add(gfx.Background.from_color("#888"))
-atlas_camera = gfx.OrthographicCamera(100, 100)
-atlas_camera.local.scale_y = -1
-
-glyph_atlas = gfx.utils.text.glyph_atlas
-
-atlas_viewer = gfx.Mesh(
-    gfx.plane_geometry(100, 100),
-    gfx.MeshBasicMaterial(color="red"),
-)
-atlas_scene.add(atlas_viewer)
-
-atlas_controller = gfx.PanZoomController(atlas_camera, register_events=viewport_atlas)
-
+# Create the text first so that the atlas glyphs are generated
 text_scene = gfx.Scene()
 text_scene.add(gfx.Background.from_color("#888"))
 text_camera = gfx.OrthographicCamera(100, 100)
@@ -70,6 +50,20 @@ text_scene.add(text_object)
 
 text_controller = gfx.PanZoomController(text_camera, register_events=viewport_text)
 
+atlas_scene = gfx.Scene()
+atlas_scene.add(gfx.Background.from_color("#888"))
+atlas_camera = gfx.OrthographicCamera(100, 100)
+
+glyph_atlas = gfx.utils.text.glyph_atlas
+
+atlas_viewer = gfx.Mesh(
+    gfx.plane_geometry(100, 100),
+    gfx.MeshBasicMaterial(color="red"),
+)
+atlas_scene.add(atlas_viewer)
+
+atlas_controller = gfx.PanZoomController(atlas_camera, register_events=viewport_atlas)
+
 gui_renderer = ImguiRenderer(renderer.device, canvas)
 
 
@@ -82,21 +76,14 @@ def draw_imgui():
 
     is_expand, _ = imgui.begin("Controls", None)
     # I can't get line inputs to work....
-    changed, text = imgui.input_text("Characters to Display", text_to_display, 100)
+    changed, text = imgui.input_text("Text", text_to_display, 100)
     if changed:
         print(text)
         text_object.text = text
         text_to_display = text
-    # Creata float slider for the font size
-    imgui.text("Font size:")
-    changed, font_size = imgui.slider_float(
-        "##font_size", text_geometry.font_size, 1, 100
-    )
-    if changed:
-        text_geometry.font_size = font_size
 
     changed, outline_thickness = imgui.slider_float(
-        "Outline Thickness", text_material.outline_thickness, 0, 1.0
+        "Outline", text_material.outline_thickness, 0, 0.5
     )
     if changed:
         text_material.outline_thickness = outline_thickness
@@ -112,6 +99,11 @@ def animate():
 
     viewport_atlas.rect = (0, 0, w // 2, h)
     viewport_text.rect = (w // 2, 0, w - w // 2, h)
+
+    # Update the image displayed on the atlas viewer
+    # TODO: how do we make this do "nearest neighbor" interpolation??
+    if atlas_viewer.material.map is not glyph_atlas.texture:
+        atlas_viewer.material.map = glyph_atlas.texture
 
     viewport_atlas.render(atlas_scene, atlas_camera)
     viewport_text.render(text_scene, text_camera)
