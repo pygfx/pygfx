4253c8865cf4ff1362da4b0b76c2239bd98c69a8
Almar Klein <almar@almarklein.org>
2025-03-06 16:18:45 +0100

    Bump version

--- pygfx/_version.py
+++ pygfx/_version.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 # This is the reference version number, to be bumped before each release.
 # The build system detects this definition when building a distribution.
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 # Allow using nearly the same code in different projects
 project_name = "pygfx"
