e92b3477d7855d3b35965eefd55a4fbadc9f1212
panxinmiao <pan_xinmiao@163.com>
2025-04-08 17:54:25 +0800

    format

--- examples/other/ray_tracing/scene.py
+++ examples/other/ray_tracing/scene.py
@@ -1,40 +1,60 @@
 import numpy as np
 import pygfx as gfx
 
+
 class Sphere(np.ndarray):
     def __new__(cls, *args, **kwargs):
-        return np.zeros(1, dtype=np.dtype([
-            ('center', np.float32, 3),
-            ('radius', np.float32),
-            ('material_index', np.uint32),
-            ('__padding', np.uint32, 3),
-        ])).view(cls)
-    
+        return np.zeros(
+            1,
+            dtype=np.dtype(
+                [
+                    ("center", np.float32, 3),
+                    ("radius", np.float32),
+                    ("material_index", np.uint32),
+                    ("__padding", np.uint32, 3),
+                ]
+            ),
+        ).view(cls)
+
     def __init__(self, center=(0, 0, 0), radius=1, material_index=0):
         super().__init__()
         self["center"] = center
         self["radius"] = radius
         self["material_index"] = material_index
 
+
 class Triangle(np.ndarray):
     def __new__(cls, *args, **kwargs):
-        return np.zeros(1, dtype=np.dtype([
-            ('p0', np.float32, 3),
-            ('__padding_0', np.uint32, 1),
-            ('p1', np.float32, 3),
-            ('__padding_1', np.uint32, 1),
-            ('p2', np.float32, 3),
-            ('__padding_2', np.uint32, 1),
-            ('n0', np.float32, 3),
-            ('__padding_3', np.uint32, 1),
-            ('n1', np.float32, 3),
-            ('__padding_4', np.uint32, 1),
-            ('n2', np.float32, 3),
-            ('material_index', np.uint32),
-        ])).view(cls)
+        return np.zeros(
+            1,
+            dtype=np.dtype(
+                [
+                    ("p0", np.float32, 3),
+                    ("__padding_0", np.uint32, 1),
+                    ("p1", np.float32, 3),
+                    ("__padding_1", np.uint32, 1),
+                    ("p2", np.float32, 3),
+                    ("__padding_2", np.uint32, 1),
+                    ("n0", np.float32, 3),
+                    ("__padding_3", np.uint32, 1),
+                    ("n1", np.float32, 3),
+                    ("__padding_4", np.uint32, 1),
+                    ("n2", np.float32, 3),
+                    ("material_index", np.uint32),
+                ]
+            ),
+        ).view(cls)
 
-    
-    def __init__(self, v0=(0, 0, 0), v1=(0, 0, 0), v2=(0, 0, 0), n0=(0, 0, 0), n1=(0, 0, 0), n2=(0, 0, 0), material_index=0):
+    def __init__(
+        self,
+        v0=(0, 0, 0),
+        v1=(0, 0, 0),
+        v2=(0, 0, 0),
+        n0=(0, 0, 0),
+        n1=(0, 0, 0),
+        n2=(0, 0, 0),
+        material_index=0,
+    ):
         super().__init__()
         self["p0"] = v0
         self["p1"] = v1
@@ -43,20 +63,27 @@ class Triangle(np.ndarray):
         self["n1"] = n1
         self["n2"] = n2
         self["material_index"] = material_index
-    
+
+
 class Material(np.ndarray, gfx.Material):
     def __new__(cls, *args, **kwargs):
-        return np.zeros(1, dtype=np.dtype([
-            ('color', np.float32, 3),
-            ("metallic", np.float32),
-            ('emissive', np.float32, 3),
-            ('roughness', np.float32),
-            ('ior', np.float32),
-            ('__padding', np.uint32, 3),
-        ])).view(cls)
+        return np.zeros(
+            1,
+            dtype=np.dtype(
+                [
+                    ("color", np.float32, 3),
+                    ("metallic", np.float32),
+                    ("emissive", np.float32, 3),
+                    ("roughness", np.float32),
+                    ("ior", np.float32),
+                    ("__padding", np.uint32, 3),
+                ]
+            ),
+        ).view(cls)
 
-    
-    def __init__(self, color=(1, 1, 1), metallic=0, emissive=(0, 0, 0), roughness=1.0, ior=0.0):
+    def __init__(
+        self, color=(1, 1, 1), metallic=0, emissive=(0, 0, 0), roughness=1.0, ior=0.0
+    ):
         super().__init__()
         self["color"] = color
         self["metallic"] = metallic
@@ -68,7 +95,8 @@ class Material(np.ndarray, gfx.Material):
 def parse_gfx_scene(s: gfx.Scene):
     materials = []
     triangles = []
-    materials_cache =[]
+    materials_cache = []
+
     def parse_node(node):
         if isinstance(node, gfx.Mesh):
             world_matrix = node.world.matrix
@@ -79,7 +107,9 @@ def parse_gfx_scene(s: gfx.Scene):
             vertices = geometry.positions.data
             normals = geometry.normals.data
 
-            vertices = np.hstack((vertices, np.ones((vertices.shape[0], 1), dtype=vertices.dtype)))
+            vertices = np.hstack(
+                (vertices, np.ones((vertices.shape[0], 1), dtype=vertices.dtype))
+            )
             vertices = (world_matrix @ vertices.T).T[:, :3]
 
             normals = (normal_matrix @ normals.T).T
@@ -100,13 +130,12 @@ def parse_gfx_scene(s: gfx.Scene):
                 v2 = vertices[indices[i][2]]
 
                 n0 = normals[indices[i][0]]
-                n1 = normals[indices[i][1]] 
+                n1 = normals[indices[i][1]]
                 n2 = normals[indices[i][2]]
 
                 triangle = Triangle(v0, v1, v2, n0, n1, n2, material_index)
 
                 triangles.append(triangle)
-
 
     s.traverse(parse_node)
 
@@ -129,7 +158,6 @@ def parse_gfx_scene(s: gfx.Scene):
             materials.append(Material(color, metallic, emissive, roughness))
 
     print(f"triangles: {len(triangles)}, materials: {len(materials)}")
-
 
     # concatenate arrays
     triangles_b = np.concatenate(triangles)
