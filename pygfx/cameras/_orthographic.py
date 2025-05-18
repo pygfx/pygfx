d5bbcf3c8d22201f28deece0109a4dddf9f1add4
Almar Klein <almar@almarklein.org>
2025-05-18 20:20:28 +0200

    Improvements to cameras (#1085)
    * Camera remembers extent when show_xx() is called, to avoid depth issues
    
    * Ruler no longer has a bbox, to avoid it breaking camera.show_object
    
    * Don't disple ortho camera's in depth-direction for zoom
    
    * Introduce depth_scale (instead of depth_multiplier)
    
    * nope, split depth_scale back up in depth_multiplier and depth_extent
    
    * Notes and tweak order of args
    
    * Fix shadow
    
    * Make sure result is the same
    
    * Set deph_range for shadow camera
    
    * remove breakpoint
    
    * Remove depth_multiplier again
    
    * Tweak auto-depth calculations
    
    * add comment
    
    * Add new screenshots
    
    * Tweak comment

--- pygfx/cameras/_orthographic.py
+++ pygfx/cameras/_orthographic.py
@@ -5,30 +5,47 @@ class OrthographicCamera(PerspectiveCamera):
     """An orthographic camera, useful for orthograpic views and 2D content.
 
     Technically, this is a PerspectiveCamera with the fov locked to zero.
+    You can use this instead of a PerspectiveCamera if you have data that does
+    not make sense to view with a nonzero fov.
+
+    When used with the show methods and/or a controller, the ``OrthographicCamera``'s
+    position does not move along the view-axis (to zoom in/out), unlike the
+    ``PerspectiveCamera``, which does this even when fov is 0.
 
     Parameters
     ----------
     width: float
         The (minimum) width of the view-cube. The actual view
-        may be wider if the viewport is relatively wide.
+        may be wider if the viewport is relatively wide. Default 1.
     height: float
         The (minimum) height of the view-cube. The actual view
-        may be taller if the viewport is relatively high.
+        may be taller if the viewport is relatively high. Default 1.
     zoom: float
         An additional zoom factor, equivalent to attaching a zoom lens.
     maintain_aspect: bool
         Whether the aspect ration is maintained as the window size changes.
         Default True. If false, the dimensions are stretched to fit the window.
-    depth_range: 2-tuple
-        The values for the near and far clipping planes. If not given
-        or None, the clip planes will be calculated automatically based
-        on the fov, width, and height.
+     depth: float | None
+        The reference size of the scene in the depth dimension. By default this value gets set
+        to the average of ``width`` and ``height``. This happens on initialization and by ``show_pos()``, ``show_object()`` and ``show_rect()``.
+        It is used to calculate a sensible depth range when ``depth_range`` is not set.
+    depth_range: 2-tuple | None
+        The explicit values for the near and far clip planes. If not given
+        or None (the default), the clip planes ware calculated using ``fov`` and ``depth``.
+
     """
 
-    _fov_range = 0, 0
+    _fov_range = 0, 0  # Disallow fov other than zero
 
     def __init__(
-        self, width=1, height=1, *, zoom=1, maintain_aspect=True, depth_range=None
+        self,
+        width=1,
+        height=1,
+        *,
+        zoom=1,
+        maintain_aspect=True,
+        depth=None,
+        depth_range=None,
     ):
         super().__init__(
             0,
@@ -37,5 +54,6 @@ class OrthographicCamera(PerspectiveCamera):
             height=height,
             zoom=zoom,
             maintain_aspect=maintain_aspect,
+            depth=depth,
             depth_range=depth_range,
         )
