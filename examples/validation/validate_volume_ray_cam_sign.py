cedddecdfa38f35cd1fe522a7e700a6c2634afb1
Gabriel Selzer <gjselzer@wisc.edu>
2026-06-12 17:42:47 -0500

    Format

--- examples/validation/validate_volume_ray_cam_sign.py
+++ examples/validation/validate_volume_ray_cam_sign.py
@@ -24,17 +24,22 @@ data = np.arange(27, dtype=np.uint16).reshape(3, 3, 3)
 scene = gfx.Scene()
 vol = gfx.Volume(
     gfx.Geometry(grid=gfx.Texture(data, dim=3)),
-    gfx.VolumeMipMaterial(clim=(float(data.min()), float(data.max())), interpolation="nearest"),
+    gfx.VolumeMipMaterial(
+        clim=(float(data.min()), float(data.max())), interpolation="nearest"
+    ),
 )
 scene.add(vol)
 
 camera = gfx.PerspectiveCamera(fov=70, depth_range=(1, 1_000_000))
-camera.world.matrix = np.array([
-    [0, 0, 1, 10],
-    [1, 0, 0,  1],
-    [0, 1, 0,  1],
-    [0, 0, 0,  1],
-], dtype=float)
+camera.world.matrix = np.array(
+    [
+        [0, 0, 1, 10],
+        [1, 0, 0, 1],
+        [0, 1, 0, 1],
+        [0, 0, 0, 1],
+    ],
+    dtype=float,
+)
 
 
 def animate():
